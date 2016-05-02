4cc33a24bfcbc054ea5f7db047bbd7593a34b942
Dan Pasanen <invisiblek@cyanogenmod.org>
2016-05-02 09:34:21 -0500

    SystemUI: Fix toggling lockscreen rotation [1/3]
    Change-Id: I28fea0124c91b8efc206a940761e77fa61d1fefc
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -129,7 +129,8 @@ public class SystemSettings {
                 Settings.System.PREFERRED_REGION,
                 Settings.System.CV_ENABLED,
                 Settings.System.CV_DYNAMIC_ENABLED,
-                Settings.System.CV_PREFERRED_INTENSITY
+                Settings.System.CV_PREFERRED_INTENSITY,
+                Settings.System.LOCKSCREEN_ROTATION
         ));
         if (Flags.backUpSmoothDisplayAndForcePeakRefreshRate()) {
             settings.add(Settings.System.PEAK_REFRESH_RATE);
