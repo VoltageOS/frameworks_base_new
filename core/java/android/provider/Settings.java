ddb6b50a7033565bc3b5f7867db4fc3a187be2d3
althafvly <althafvly@gmail.com>
2020-05-26 21:17:59 +0800

    SystemUI: support black theme for dark mode [1/4]
    aosip edit: use the black theme we already have
    
    Also includes squashed change:
    
    Author: Michael Bestas <mkbestas@gmail.com>
    Date:   Mon Sep 26 17:29:51 2022 +0300
    
        fixup! SystemUI: support black theme for dark mode [1/4]
    
        Fixes color picker
    
        Change-Id: If29713907a836384c024a4b66ea461acd97f2267
    
    Co-authored-by: Jesse Chan <jc@lineageos.org>
    Change-Id: I57cd53de8f2c1e4d445441b514875b6af915b858
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -13656,6 +13656,12 @@ public final class Settings {
          * @hide
          */
         public static final String AAPM_USB_DATA_PROTECTION = "aapm_usb_data_protection";
+
+	/**
+         * Whether to use black theme for dark mode
+         * @hide
+         */
+        public static final String BERRY_BLACK_THEME = "berry_black_theme";
     }
 
     /**
