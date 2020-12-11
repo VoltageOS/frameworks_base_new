d8c4c1ed5421e5ba874868450fd1c54fe2c86a1e
Pierre-Hugues Husson <phh@phh.me>
2020-12-11 14:41:09 +0100

    Remove useless notification about "console" service being running
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -5296,7 +5296,7 @@ public class ActivityManagerService extends IActivityManager.Stub
     }
 
     private void showConsoleNotificationIfActive() {
-        if (!isUartEnabled()) {
+        if (!SystemProperties.get("init.svc.console").equals("running") || true) {
             return;
         }
         String title = mContext
