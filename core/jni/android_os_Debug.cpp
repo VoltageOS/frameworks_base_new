Neoteric-OS/android_frameworks_base

Extend getPssPid to report itemized memtrack values
Currently getPssPid includes memtrack HAL reported values in the pss
calculation. This makes it impossible to get these values separately.
Extend this interface to include itemized memtrack values.

Bug: 165832481
Test: dumpsys meminfo
Signed-off-by: Suren Baghdasaryan <surenb@google.com>
Change-Id: Ieb7e1b79ad8ecae6023338d9992467952558e2bd

core/jni/android_os_Debug.cpp
@@ -540,6 +540,15 @@ static jlong android_os_Debug_getPssPid(JNIEnv *env, jobject clazz, jint pid,
             jlong* outMemtrackArray = env->GetLongArrayElements(outMemtrack, 0);
             if (outMemtrackArray != NULL) {
                 outMemtrackArray[0] = memtrack;
+                if (env->GetArrayLength(outUssSwapPssRss) >= 2) {
+                    outMemtrackArray[1] = graphics_mem.graphics;
+                }
+                if (env->GetArrayLength(outUssSwapPssRss) >= 3) {
+                    outMemtrackArray[2] = graphics_mem.gl;
+                }
+                if (env->GetArrayLength(outUssSwapPssRss) >= 4) {
+                    outMemtrackArray[3] = graphics_mem.other;
+                }
             }
             env->ReleaseLongArrayElements(outMemtrack, outMemtrackArray, 0);
         }

VoltageOS/frameworks_base_new

core/jni/android_os_Debug.cpp
@@ -540,6 +540,15 @@ static jlong android_os_Debug_getPssPid(JNIEnv *env, jobject clazz, jint pid,
             jlong* outMemtrackArray = env->GetLongArrayElements(outMemtrack, 0);
             if (outMemtrackArray != NULL) {
                 outMemtrackArray[0] = memtrack;
+                if (env->GetArrayLength(outMemtrack) >= 2) {
+                    outMemtrackArray[1] = graphics_mem.graphics;
+                }
+                if (env->GetArrayLength(outMemtrack) >= 3) {
+                    outMemtrackArray[2] = graphics_mem.gl;
+                }
+                if (env->GetArrayLength(outMemtrack) >= 4) {
+                    outMemtrackArray[3] = graphics_mem.other;
+                }
             }
             env->ReleaseLongArrayElements(outMemtrack, outMemtrackArray, 0);
         }