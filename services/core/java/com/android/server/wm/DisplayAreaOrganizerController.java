VoltageOS/frameworks_base_new

commit 84818d16988ba20574fe426dac3dc108ba9a4ab2

Merge Stack level of window hierarchy (63/n)
Bug: 80414790
Test: Existing tests pass
Change-Id: Ia6765f7ebd89b38e0d2b88ed1cdfec5b386a0774

services/core/java/com/android/server/wm/DisplayContent.java
@@ -937,6 +937,8 @@ class DisplayContent extends WindowContainer<DisplayContent.DisplayChildWindowCo
         super.addChild(mTaskStackContainers, null);
         super.addChild(mAboveAppWindowsContainers, null);
         super.addChild(mImeWindowsContainers, null);
+        // Sets the display content for the children.
+        onDisplayChanged(this);
 
         // Add itself as a child to the root container.
         mWmService.mRoot.addChild(this, null);
@@ -2202,27 +2204,18 @@ class DisplayContent extends WindowContainer<DisplayContent.DisplayChildWindowCo
         out.set(mDisplayFrames.mStable);
     }
 
-    void setStackOnDisplay(int stackId, boolean onTop, TaskStack stack) {
-        if (DEBUG_STACK) {
-            Slog.d(TAG_WM, "Create new stackId=" + stackId + " on displayId=" + mDisplayId);
-        }
-
-        mTaskStackContainers.addStackToDisplay(stack, onTop);
+    void setStackOnDisplay(TaskStack stack, int position) {
+        if (DEBUG_STACK) Slog.d(TAG_WM, "Set stack=" + stack + " on displayId=" + mDisplayId);
+        mTaskStackContainers.addChild(stack, position);
     }
 
     void moveStackToDisplay(TaskStack stack, boolean onTop) {
-        final DisplayContent prevDc = stack.getDisplayContent();
-        if (prevDc == null) {
-            throw new IllegalStateException("Trying to move stackId=" + stack.mStackId
-                    + " which is not currently attached to any display");
-        }
-        if (prevDc.getDisplayId() == mDisplayId) {
-            throw new IllegalArgumentException("Trying to move stackId=" + stack.mStackId
-                    + " to its current displayId=" + mDisplayId);
-        }
+        stack.reparent(mTaskStackContainers, onTop ? POSITION_TOP: POSITION_BOTTOM);
+    }
 
-        prevDc.mTaskStackContainers.removeChild(stack);
-        mTaskStackContainers.addStackToDisplay(stack, onTop);
+    // TODO(display-unify): No longer needed then.
+    void removeStackFromDisplay(TaskStack stack) {
+        mTaskStackContainers.removeChild(stack);
     }
 
     @Override
@@ -2357,8 +2350,8 @@ class DisplayContent extends WindowContainer<DisplayContent.DisplayChildWindowCo
     }
 
     @Override
-    void switchUser() {
-        super.switchUser();
+    void switchUser(int userId) {
+        super.switchUser(userId);
         mWmService.mWindowsChanged = true;
         mDisplayPolicy.switchUser();
     }
@@ -2624,7 +2617,7 @@ class DisplayContent extends WindowContainer<DisplayContent.DisplayChildWindowCo
         proto.write(ID, mDisplayId);
         for (int stackNdx = mTaskStackContainers.getChildCount() - 1; stackNdx >= 0; --stackNdx) {
             final TaskStack stack = mTaskStackContainers.getChildAt(stackNdx);
-            stack.writeToProto(proto, STACKS, logLevel);
+            stack.writeToProtoInnerStackOnly(proto, STACKS, logLevel);
         }
         mDividerControllerLocked.writeToProto(proto, DOCKED_STACK_DIVIDER_CONTROLLER);
         mPinnedStackControllerLocked.writeToProto(proto, PINNED_STACK_CONTROLLER);
@@ -3947,6 +3940,12 @@ class DisplayContent extends WindowContainer<DisplayContent.DisplayChildWindowCo
             super(service);
         }
 
+        @Override
+        public void onConfigurationChanged(Configuration newParentConfig) {
+            // TODO(display-unify): Remove after unification.
+            onConfigurationChanged(newParentConfig, mActivityDisplay == null /*forwardToChildren*/);
+        }
+
         /**
          * Returns the topmost stack on the display that is compatible with the input windowing mode
          * and activity type. Null is no compatible stack on the display.
@@ -4006,15 +4005,6 @@ class DisplayContent extends WindowContainer<DisplayContent.DisplayChildWindowCo
             return visibleTasks;
         }
 
-        /**
-         * Adds the stack to this container.
-         */
-        void addStackToDisplay(TaskStack stack, boolean onTop) {
-            addStackReferenceIfNeeded(stack);
-            addChild(stack, onTop);
-            stack.onDisplayChanged(DisplayContent.this);
-        }
-
         void onStackWindowingModeChanged(TaskStack stack) {
             removeStackReferenceIfNeeded(stack);
             addStackReferenceIfNeeded(stack);
@@ -4066,16 +4056,31 @@ class DisplayContent extends WindowContainer<DisplayContent.DisplayChildWindowCo
             }
         }
 
-        private void addChild(TaskStack stack, boolean toTop) {
-            final int addIndex = findPositionForStack(toTop ? mChildren.size() : 0, stack,
-                    true /* adding */);
-            addChild(stack, addIndex);
-            setLayoutNeeded();
+        @Override
+        void addChild(TaskStack stack, int position) {
+            addStackReferenceIfNeeded(stack);
+            position = findPositionForStack(position, stack, true /* adding */);
+
+            super.addChild(stack, position);
+            if (mActivityDisplay != null) {
+                // TODO(stack-merge): Remove cast.
+                mActivityDisplay.addChild((ActivityStack) stack, position, true /*fromDc*/);
+            }
+
+            // The reparenting case is handled in WindowContainer.
+            if (!stack.mReparenting) {
+                setLayoutNeeded();
+                stack.onDisplayChanged(DisplayContent.this);
+            }
         }
 
         @Override
         protected void removeChild(TaskStack stack) {
             super.removeChild(stack);
+            if (mActivityDisplay != null) {
+                // TODO(stack-merge): Remove cast.
+                mActivityDisplay.onChildRemoved((ActivityStack) stack);
+            }
             removeStackReferenceIfNeeded(stack);
         }
 
@@ -4161,6 +4166,14 @@ class DisplayContent extends WindowContainer<DisplayContent.DisplayChildWindowCo
             int targetPosition = requestedPosition;
             targetPosition = Math.min(targetPosition, maxPosition);
             targetPosition = Math.max(targetPosition, minPosition);
+
+            // Cap the requested position to something reasonable for the previous position check
+            // below.
+            if (requestedPosition == POSITION_TOP) {
+                requestedPosition = mChildren.size();
+            } else if (requestedPosition == POSITION_BOTTOM) {
+                requestedPosition = 0;
+            }
 
             int prevPosition = getStacks().indexOf(stack);
             // The positions we calculated above (maxPosition, minPosition) do not take into

services/core/java/com/android/server/wm/Task.java
@@ -226,10 +226,13 @@ class Task extends WindowContainer<ActivityRecord> implements ConfigurationConta
                 + " from stack=" + getTaskStack());
         EventLog.writeEvent(WM_TASK_REMOVED, mTaskId, "reParentTask");
 
-        final ActivityStack prevStack = getTaskStack().mActivityStack;
+        // TODO(stack-merge): Remove cast.
+        final ActivityStack prevStack = (ActivityStack) getTaskStack();
         final boolean wasTopFocusedStack =
                 mAtmService.mRootActivityContainer.isTopDisplayFocusedStack(prevStack);
         final ActivityDisplay prevStackDisplay = prevStack.getDisplay();
+
+        position = stack.findPositionForTask(this, position, showForAllUsers());
 
         reparent(stack, position);
 
@@ -245,12 +248,6 @@ class Task extends WindowContainer<ActivityRecord> implements ConfigurationConta
         // our insets so that there will not be a jump in the area covered by system decorations.
         // We rely on the pinned animation to later unset this value.
         mPreserveNonFloatingState = stack.inPinnedWindowingMode();
-    }
-
-    /** @see ActivityTaskManagerService#positionTaskInStack(int, int, int). */
-    void positionAt(int position) {
-        // TODO(task-merge): Remove cast.
-        getTaskStack().positionChildAt(position, (TaskRecord) this, false /* includingParents */);
     }
 
     void setSendingToBottom(boolean toBottom) {

core/proto/android/server/activitymanagerservice.proto
@@ -88,13 +88,15 @@ message ActivityDisplayProto {
 message ActivityStackProto {
     option (.android.msg_privacy).dest = DEST_AUTOMATIC;
 
-    optional .com.android.server.wm.ConfigurationContainerProto configuration_container = 1;
+    // To be removed soon.
+    optional .com.android.server.wm.ConfigurationContainerProto configuration_container = 1 [deprecated=true];
     optional int32 id = 2;
     repeated TaskRecordProto tasks = 3;
     optional .com.android.server.wm.IdentifierProto resumed_activity = 4;
     optional int32 display_id = 5;
     optional bool fullscreen = 6;
     optional .android.graphics.RectProto bounds = 7;
+    optional .com.android.server.wm.StackProto stack = 8;
 }
 
 message TaskRecordProto {

services/core/java/com/android/server/wm/ActivityDisplay.java
@@ -123,6 +123,9 @@ class ActivityDisplay extends ConfigurationContainer<ActivityStack> {
 
     private boolean mSleeping;
 
+    /** We started the process of removing the display from the system. */
+    private boolean mRemoving;
+
     /**
      * The display is removed from the system and we are just waiting for all activities on it to be
      * finished before removing this object.
@@ -200,23 +203,37 @@ class ActivityDisplay extends ConfigurationContainer<ActivityStack> {
         }
     }
 
+    // TODO(display-unify): Merge with addChild below.
     void addChild(ActivityStack stack, int position) {
+        addChild(stack, position, false /*fromDc*/);
+    }
+
+    void addChild(ActivityStack stack, int position, boolean fromDc) {
+        boolean toTop = position == POSITION_TOP;
         if (position == POSITION_BOTTOM) {
             position = 0;
-        } else if (position == POSITION_TOP) {
+        } else if (toTop) {
             position = mStacks.size();
         }
         if (DEBUG_STACK) Slog.v(TAG_STACK, "addChild: attaching " + stack
                 + " to displayId=" + mDisplayId + " position=" + position);
         addStackReferenceIfNeeded(stack);
+        if (!fromDc) {
+            mDisplayContent.setStackOnDisplay(stack, position);
+        }
         positionChildAt(stack, position);
         mService.updateSleepIfNeededLocked();
     }
 
-    void removeChild(ActivityStack stack) {
+    // TODO(display-unify): Merge with removeChild below.
+    void onChildRemoved(ActivityStack stack) {
+        if (!mStacks.remove(stack)) {
+            // Stack no longer here!
+            return;
+        }
+
         if (DEBUG_STACK) Slog.v(TAG_STACK, "removeChild: detaching " + stack
                 + " from displayId=" + mDisplayId);
-        mStacks.remove(stack);
         if (mPreferredTopFocusableStack == stack) {
             mPreferredTopFocusableStack = null;
         }
@@ -224,6 +241,10 @@ class ActivityDisplay extends ConfigurationContainer<ActivityStack> {
         releaseSelfIfNeeded();
         mService.updateSleepIfNeededLocked();
         onStackOrderChanged(stack);
+    }
+
+    void removeChild(ActivityStack stack) {
+        mDisplayContent.removeStackFromDisplay(stack);
     }
 
     void positionChildAtTop(ActivityStack stack, boolean includingParents) {
@@ -286,12 +307,8 @@ class ActivityDisplay extends ConfigurationContainer<ActivityStack> {
 
         // Since positionChildAt() is called during the creation process of pinned stacks,
         // ActivityStack#getStack() can be null.
-        if (stack.getTaskStack() != null && mDisplayContent != null) {
-            mDisplayContent.positionStackAt(insertPosition,
-                    stack.getTaskStack(), includingParents);
-        }
-        if (!wasContained) {
-            stack.setParent(this);
+        if (mDisplayContent != null) {
+            mDisplayContent.positionStackAt(insertPosition, stack, includingParents);
         }
         onStackOrderChanged(stack);
     }
@@ -1186,7 +1203,15 @@ class ActivityDisplay extends ConfigurationContainer<ActivityStack> {
         return mRemoved;
     }
 
+    /**
+     * @see #mRemoving
+     */
+    boolean isRemoving() {
+        return mRemoving;
+    }
+
     void remove() {
+        mRemoving = true;
         final boolean destroyContentOnRemoval = shouldDestroyContentOnRemove();
         ActivityStack lastReparentedStack = null;
         mPreferredTopFocusableStack = null;
@@ -1213,7 +1238,7 @@ class ActivityDisplay extends ConfigurationContainer<ActivityStack> {
                     final int windowingMode = toDisplay.hasSplitScreenPrimaryStack()
                             ? WINDOWING_MODE_SPLIT_SCREEN_SECONDARY
                             : WINDOWING_MODE_UNDEFINED;
-                    stack.reparent(toDisplay, true /* onTop */, true /* displayRemoved */);
+                    stack.reparent(toDisplay.mDisplayContent, true /* onTop */);
                     stack.setWindowingMode(windowingMode);
                     lastReparentedStack = stack;
                 }
@@ -1251,7 +1276,7 @@ class ActivityDisplay extends ConfigurationContainer<ActivityStack> {
             // Release this display if an empty home stack is the only thing left.
             // Since it is the last stack, this display will be released along with the stack
             // removal.
-            stack.remove();
+            stack.removeIfPossible();
         } else if (mStacks.isEmpty()) {
             mDisplayContent.removeIfPossible();
             mDisplayContent = null;

services/core/java/com/android/server/wm/ActivityRecord.java
@@ -42,6 +42,8 @@ import static android.app.WindowConfiguration.ACTIVITY_TYPE_RECENTS;
 import static android.app.WindowConfiguration.ACTIVITY_TYPE_UNDEFINED;
 import static android.app.WindowConfiguration.ROTATION_UNDEFINED;
 import static android.app.WindowConfiguration.WINDOWING_MODE_FREEFORM;
+import static android.app.WindowConfiguration.WINDOWING_MODE_FULLSCREEN;
+import static android.app.WindowConfiguration.WINDOWING_MODE_PINNED;
 import static android.app.WindowConfiguration.WINDOWING_MODE_SPLIT_SCREEN_PRIMARY;
 import static android.app.WindowConfiguration.activityTypeToString;
 import static android.content.Intent.ACTION_MAIN;
@@ -1992,9 +1994,8 @@ final class ActivityRecord extends WindowToken implements WindowManagerService.A
     /**
      * @return Stack value from current task, null if there is no task.
      */
-    // TODO(stack-unify): Remove once ActivityStack and TaskStack are unified.
-    <T extends ActivityStack> T getActivityStack() {
-        return task != null ? (T) task.getStack() : null;
+    ActivityStack getActivityStack() {
+        return task != null ? task.getStack() : null;
     }
 
     int getStackId() {
@@ -2080,10 +2081,11 @@ final class ActivityRecord extends WindowToken implements WindowManagerService.A
         return ActivityInfo.isResizeableMode(info.resizeMode) || info.supportsPictureInPicture();
     }
 
-    /**
-     * @return whether this activity is non-resizeable or forced to be resizeable
-     */
-    boolean isNonResizableOrForcedResizable() {
+    /** @return whether this activity is non-resizeable or forced to be resizeable */
+    boolean isNonResizableOrForcedResizable(int windowingMode) {
+        if (windowingMode == WINDOWING_MODE_PINNED && info.supportsPictureInPicture()) {
+            return false;
+        }
         return info.resizeMode != RESIZE_MODE_RESIZEABLE
                 && info.resizeMode != RESIZE_MODE_RESIZEABLE_VIA_SDK_VERSION;
     }

services/core/java/com/android/server/wm/ActivityStackSupervisor.java
@@ -1713,8 +1713,8 @@ public class ActivityStackSupervisor implements RecentTasks.Callbacks {
         // another AM call that is holding the AMS lock. In such a case, the pinnedBounds may be
         // incorrect if AMS.resizeStackWithBoundsFromWindowManager() is already called while waiting
         // for the AMS lock to be freed. So check and make sure these bounds are still good.
-        final TaskStack stackController = stack.getTaskStack();
-        if (stackController.pinnedStackResizeDisallowed()) {
+        // TODO(stack-merge): Is this still relevant?
+        if (stack.pinnedStackResizeDisallowed()) {
             return;
         }
 
@@ -1893,8 +1893,7 @@ public class ActivityStackSupervisor implements RecentTasks.Callbacks {
 
         if (currentStack != null) {
             // Task has already been restored once. Just re-parent it to the new stack.
-            task.reparent(stack.mTaskStack,
-                    POSITION_TOP, true /*moveParents*/, "restoreRecentTaskLocked");
+            task.reparent(stack, POSITION_TOP, true /*moveParents*/, "restoreRecentTaskLocked");
             return true;
         }
 
@@ -2470,7 +2469,7 @@ public class ActivityStackSupervisor implements RecentTasks.Callbacks {
     private void handleForcedResizableTaskIfNeeded(TaskRecord task, int reason) {
         final ActivityRecord topActivity = task.getTopActivity();
         if (topActivity == null || topActivity.noDisplay
-                || !topActivity.isNonResizableOrForcedResizable()) {
+                || !topActivity.isNonResizableOrForcedResizable(task.getWindowingMode())) {
             return;
         }
         mService.getTaskChangeNotificationController().notifyActivityForcedResizable(

services/core/java/com/android/server/wm/ActivityStarter.java
@@ -1449,7 +1449,7 @@ class ActivityStarter {
         if (startedActivityStack != null && startedActivityStack.isAttached()
                 && startedActivityStack.numActivities() == 0
                 && !startedActivityStack.isActivityTypeHome()) {
-            startedActivityStack.remove();
+            startedActivityStack.removeIfPossible();
             startedActivityStack = null;
         }
         return startedActivityStack;
@@ -2356,7 +2356,7 @@ class ActivityStarter {
                     // The task does not need to be reparented to the launch stack. Remove the
                     // launch stack if there is no activity in it.
                     Slog.w(TAG, "Removing an empty stack: " + launchStack);
-                    launchStack.remove();
+                    launchStack.removeIfPossible();
                 }
 
                 mOptions = null;

commit 087b92ef72486e7d64e2fa0f6e5c207509a165b3

Fix black screen after moving top activity to another display
The screen was black after moving the top activity to another display.
The next top activity did not be resumed and was still invisible. The
regression was from 84818d1, which incidentally reverse the condition.

Since the case of removing a display is already handled by ec2275a,
#postReparent is not needed to call again in #reparent (when display
is removing).

Fixing the issue forward by resuming the focusable task in
#moveRootTaskToTaskDisplayArea.

Bug: 192616155
Test: atest ActivityTaskManagerServiceTests
Change-Id: Id0971ad8a1c692fdb5376e2c077faa6f00dd61ac

services/core/java/com/android/server/wm/DisplayAreaOrganizerController.java
@@ -353,7 +353,7 @@ public class DisplayAreaOrganizerController extends IDisplayAreaOrganizerControl
         // Only update focus/visibility for the last one because there may be many root tasks are
         // reparented and the intermediate states are unnecessary.
         if (lastReparentedRootTask != null) {
-            lastReparentedRootTask.postReparent();
+            lastReparentedRootTask.resumeNextFocusAfterReparent();
         }
     }
 }

services/core/java/com/android/server/wm/DisplayContent.java
@@ -5696,7 +5696,7 @@ class DisplayContent extends RootDisplayArea implements WindowManagerPolicy.Disp
         // Only update focus/visibility for the last one because there may be many root tasks are
         // reparented and the intermediate states are unnecessary.
         if (lastReparentedRootTask != null) {
-            lastReparentedRootTask.postReparent();
+            lastReparentedRootTask.resumeNextFocusAfterReparent();
         }
         releaseSelfIfNeeded();
         mDisplayPolicy.release();

services/core/java/com/android/server/wm/RootWindowContainer.java
@@ -71,7 +71,6 @@ import static com.android.server.wm.ActivityTaskSupervisor.ON_TOP;
 import static com.android.server.wm.ActivityTaskSupervisor.PRESERVE_WINDOWS;
 import static com.android.server.wm.ActivityTaskSupervisor.dumpHistoryList;
 import static com.android.server.wm.ActivityTaskSupervisor.printThisActivity;
-import static com.android.server.wm.RecentsAnimationController.REORDER_KEEP_IN_PLACE;
 import static com.android.server.wm.RootWindowContainerProto.IS_HOME_RECENTS_COMPONENT;
 import static com.android.server.wm.RootWindowContainerProto.KEYGUARD_CONTROLLER;
 import static com.android.server.wm.RootWindowContainerProto.WINDOW_CONTAINER;
@@ -2078,6 +2077,10 @@ class RootWindowContainer extends WindowContainer<DisplayContent>
                     + " to its current taskDisplayArea=" + taskDisplayArea);
         }
         rootTask.reparent(taskDisplayArea, onTop);
+
+        // Resume focusable root task after reparenting to another display area.
+        rootTask.resumeNextFocusAfterReparent();
+
         // TODO(multi-display): resize rootTasks properly if moved from split-screen.
     }
 

services/core/java/com/android/server/wm/Task.java
@@ -1231,12 +1231,6 @@ class Task extends TaskFragment {
         adjustBoundsForDisplayChangeIfNeeded(getDisplayContent());
 
         mRootWindowContainer.updateUIDsPresentOnDisplay();
-
-        // Resume next focusable root task after reparenting to another display if we aren't
-        // removing the prevous display.
-        if (oldDisplay != null && oldDisplay.isRemoving()) {
-            postReparent();
-        }
     }
 
     /** Returns the currently topmost resumed activity. */
@@ -4514,8 +4508,7 @@ class Task extends TaskFragment {
         mRootWindowContainer.resumeFocusedTasksTopActivities();
     }
 
-    /** Resume next focusable root task after reparenting to another display. */
-    void postReparent() {
+    void resumeNextFocusAfterReparent() {
         adjustFocusToNextFocusableTask("reparent", true /* allowFocusSelf */,
                 true /* moveDisplayToTop */);
         mRootWindowContainer.resumeFocusedTasksTopActivities();