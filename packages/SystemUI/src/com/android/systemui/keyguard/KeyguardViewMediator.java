f1d08ec5fdb07158105c9411d3126b51d833e7a6
Matt Pietal <mpietal@google.com>
2021-10-01 11:03:16 -0400

    Keyguard - Treat messages to lock with priority
    When switching users and attempting to lock the device, the sysui main
    thread becomes overwhelmed with events, creating a significant lag
    between the time a message is posted and processed on the main
    thread. This can be dangerous when these events are critical for
    security, such as calls coming from PhoneWindowManager#lockNow() that
    call KeyguardViewMediator#doKeyguardTimeout(). On older devices with
    slower CPUs and less memory, the delay in processing can be
    significant (15 - 30s).
    
    The result of not prioritizing these events leads to a window of time
    where a guest user can switch back to the owner, and gain access to
    the owner's homescreen without needing to unlock the device with the
    owner's credentials.
    
    As a mitigation, prioritize two events originating in two specific
    methods to make sure the device locks as soon as possible as well as
    have the system server preemptively update its local cache.
    
    Bug: 151095871
    Test: Very manual race condition - follow steps listed in bug
    Change-Id: I7585a0a5eeb308e0e32a4f77f581556d883b5cda
    Merged-In: I7585a0a5eeb308e0e32a4f77f581556d883b5cda
    (cherry picked from commit ffe74d051d1d627b55cc2384ddcdbb85eac2e8e9)
    (cherry picked from commit 86f736576db5b66b8a639fc5fbf6364ba8aa020a)
    (cherry picked from commit 7d1fabebc954e0d091e66283ff6153543fa48606)
    Merged-In: I7585a0a5eeb308e0e32a4f77f581556d883b5cda
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
+++ packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
@@ -2990,6 +2990,7 @@ public class KeyguardViewMediator implements CoreStartable,
                     message = "KEYGUARD_TIMEOUT";
                     synchronized (KeyguardViewMediator.this) {
                         doKeyguardLocked((Bundle) msg.obj);
+                        notifyDefaultDisplayCallbacks(mShowing);
                     }
                     break;
                 case DISMISS:
