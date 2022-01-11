ed247ba13eebb3fc40680f55f93f827d9c45e99e
jhonboy121 <alfredmathew05@gmail.com>
2022-01-11 20:43:24 +0530

    Allow disabling qs on secure lockscreen [1/2]
    * This does not prevent pulling down notification shade,
      qs will be entirely hidden even if you do and that's it.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesCommandQueueCallbacks.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesCommandQueueCallbacks.java
@@ -75,6 +75,7 @@ import com.android.systemui.statusbar.policy.FlashlightController;
 import com.android.systemui.statusbar.policy.KeyguardStateController;
 import com.android.systemui.statusbar.policy.RemoteInputQuickSettingsDisabler;
 import com.android.systemui.wallet.controller.QuickAccessWalletController;
+import com.android.systemui.statusbar.policy.SecureLockscreenQSDisabler;
 
 import dagger.Lazy;
 
@@ -118,6 +119,8 @@ public class CentralSurfacesCommandQueueCallbacks implements CommandQueue.Callba
     private final QuickSettingsController mQsController;
     private final QSHost mQSHost;
     private final KeyguardInteractor mKeyguardInteractor;
+    private final SecureLockscreenQSDisabler mSecureLockscreenQSDisabler;
+
     private static final VibrationAttributes HARDWARE_FEEDBACK_VIBRATION_ATTRIBUTES =
             VibrationAttributes.createForUsage(VibrationAttributes.USAGE_HARDWARE_FEEDBACK);
 
@@ -165,7 +168,8 @@ public class CentralSurfacesCommandQueueCallbacks implements CommandQueue.Callba
             ActivityStarter activityStarter,
             KeyguardInteractor keyguardInteractor,
             EmergencyGestureIntentFactory emergencyGestureIntentFactory,
-            QuickAccessWalletController walletController) {
+            QuickAccessWalletController walletController,
+            SecureLockscreenQSDisabler secureLockscreenQSDisabler) {
         mCentralSurfaces = centralSurfaces;
         mQsController = quickSettingsController;
         mContext = context;
@@ -195,6 +199,8 @@ public class CentralSurfacesCommandQueueCallbacks implements CommandQueue.Callba
         mUserTracker = userTracker;
         mQSHost = qsHost;
         mKeyguardInteractor = keyguardInteractor;
+        mSecureLockscreenQSDisabler = secureLockscreenQSDisabler;
+
         mVibrateOnOpening = resources.getBoolean(R.bool.config_vibrateOnIconAnimation);
         mCameraLaunchGestureVibrationEffect = getCameraGestureVibrationEffect(
                 mVibratorOptional, resources);
@@ -293,6 +299,7 @@ public class CentralSurfacesCommandQueueCallbacks implements CommandQueue.Callba
         mDisabled1 = state1;
 
         state2 = mRemoteInputQuickSettingsDisabler.adjustDisableFlags(state2);
+        state2 = mSecureLockscreenQSDisabler.adjustDisableFlags(state2);
         final int old2 = mDisabled2;
         final int diff2 = state2 ^ old2;
         mDisabled2 = state2;
