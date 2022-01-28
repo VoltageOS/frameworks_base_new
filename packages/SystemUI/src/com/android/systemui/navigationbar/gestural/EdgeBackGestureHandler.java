06127f7f6460e94dc28bacb2a6423354194c3a42
kdrag0n <danny@kdrag0n.dev>
2022-01-28 17:54:02 +0100

    Gesture nav: Add Power menu action
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Change-Id: I11cbb003a03b17a981d957cf25aa28138426802d
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- packages/SystemUI/src/com/android/systemui/navigationbar/gestural/EdgeBackGestureHandler.java
+++ packages/SystemUI/src/com/android/systemui/navigationbar/gestural/EdgeBackGestureHandler.java
@@ -1430,6 +1430,9 @@ public class EdgeBackGestureHandler implements PluginListener<NavigationEdgeBack
             case 13: // Switch recent app
                 VoltageUtils.switchToLastApp(mContext);
                 break;
+            case 14: // Powermenu
+                VoltageUtils.showPowerMenu();
+                break;
         }
     }
 
