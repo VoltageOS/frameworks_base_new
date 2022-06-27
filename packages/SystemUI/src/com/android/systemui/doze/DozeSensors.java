223f8c22e57cbc63943ea9884ffe9a5a0f326f08
Cosmin Tanislav <demonsingur@gmail.com>
2022-06-27 16:51:50 +0300

    SystemUI: doze: add config for double tap sensors that need a proximity check
    Change-Id: Id7df987f0e9db4e5e2328534764ded397b6ac48d
    
    -----
    
    SystemUI: doze: add config for long press sensors that need a proximity check
    
    Change-Id: I34d5a8313304e3ac8bd53282de66ab1d3d1653a8
    Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- packages/SystemUI/src/com/android/systemui/doze/DozeSensors.java
+++ packages/SystemUI/src/com/android/systemui/doze/DozeSensors.java
@@ -210,7 +210,8 @@ public class DozeSensors {
                         true /* configured */,
                         DozeLog.REASON_SENSOR_DOUBLE_TAP,
                         dozeParameters.doubleTapReportsTouchCoordinates(),
-                        true /* touchscreen */
+                        true /* touchscreen */,
+                        !dozeParameters.doubleTapNeedsProximityCheck() /* performsProxCheck */
                 ),
                 new TriggerSensor(
                         findSensors(config.tapSensorTypeMapping()),
@@ -236,6 +237,7 @@ public class DozeSensors {
                         true /* touchscreen */,
                         false /* ignoresSetting */,
                         dozeParameters.longPressUsesProx() /* requiresProx */,
+                        !dozeParameters.longPressNeedsProximityCheck() /* performsProxCheck */,
                         true /* immediatelyReRegister */,
                         false /* requiresAod */
                 ),
@@ -249,6 +251,7 @@ public class DozeSensors {
                         true /* touchscreen */,
                         false /* ignoresSetting */,
                         dozeParameters.longPressUsesProx(),
+                        !dozeParameters.longPressNeedsProximityCheck() /* performsProxCheck */,
                         screenOffUnlockUdfps() /* immediatelyReRegister */,
                         !screenOffUnlockUdfps() /* requiresAod */
                 ),
