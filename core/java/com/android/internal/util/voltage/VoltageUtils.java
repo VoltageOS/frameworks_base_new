6c62bc077cf9b59c3b736e377be494069cababa3
shivatejapeddi <shivatejapeddi@gmail.com>
2022-10-30 19:23:31 +0100

    VoltageUtils: Add isPackageAvailable method
    Change-Id: I6d97e18ec4fc260264145caadafa81792ff3e8e8
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- core/java/com/android/internal/util/voltage/VoltageUtils.java
+++ core/java/com/android/internal/util/voltage/VoltageUtils.java
@@ -171,6 +171,18 @@ public class VoltageUtils {
         return isPackageInstalled(context, pkg, true);
     }
 
+    public static boolean isPackageAvailable(Context context, String packageName) {
+        final PackageManager pm = context.getPackageManager();
+        try {
+            pm.getPackageInfo(packageName, PackageManager.GET_ACTIVITIES);
+            int enabled = pm.getApplicationEnabledSetting(packageName);
+            return enabled != PackageManager.COMPONENT_ENABLED_STATE_DISABLED &&
+                enabled != PackageManager.COMPONENT_ENABLED_STATE_DISABLED_USER;
+        } catch (NameNotFoundException e) {
+            return false;
+        }
+    }
+
     public static boolean deviceSupportsFlashLight(Context context) {
         CameraManager cameraManager = (CameraManager) context.getSystemService(
                 Context.CAMERA_SERVICE);
