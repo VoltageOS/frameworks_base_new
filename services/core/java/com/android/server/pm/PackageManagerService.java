9eaa8ccf20e3e1594e4b12a1e892b40840ba8976
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-01-30 19:04:30 +0200

    disable package parser cache
    This is needed for properly verifying updates of system packages.
    
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -1710,7 +1710,7 @@ public class PackageManagerService implements PackageSender, TestUtilityService 
                         () -> LocalServices.getService(UserManagerInternal.class)),
                 (i, pm) -> new DisplayMetrics(),
                 (i, pm) -> new PackageParser2(pm.mSeparateProcesses, i.getDisplayMetrics(),
-                        new PackageCacher(pm.mCacheDir, pm.mPackageParserCallback),
+                        pm.hasCacheDir() ? new PackageCacher(pm.mCacheDir, pm.mPackageParserCallback) : null,
                         pm.mPackageParserCallback) /* scanningCachingPackageParserProducer */,
                 (i, pm) -> new PackageParser2(pm.mSeparateProcesses, i.getDisplayMetrics(), null,
                         pm.mPackageParserCallback) /* scanningPackageParserProducer */,
@@ -2281,8 +2281,14 @@ public class PackageManagerService implements PackageSender, TestUtilityService 
                 t.traceEnd();
             }
 
-            mCacheDir = PackageManagerServiceUtils.preparePackageParserCache(
+            File cacheDir = PackageManagerServiceUtils.preparePackageParserCache(
                     mIsEngBuild, mIsUserDebugBuild, mIncrementalVersion);
+            if (cacheDir == null) {
+                // several places don't expect mCacheDir to be null, despite it being nullable
+                // upstream
+                cacheDir = new File("/dev/null");
+            }
+            mCacheDir = cacheDir;
 
             mInitialNonStoppedSystemPackages = mInjector.getSystemConfig()
                     .getInitialNonStoppedSystemPackages();
@@ -8000,6 +8006,10 @@ public class PackageManagerService implements PackageSender, TestUtilityService 
 
     File getCacheDir() {
         return mCacheDir;
+    }
+
+    boolean hasCacheDir() {
+        return !"/dev/null".equals(mCacheDir.getPath());
     }
 
     PackageProperty getPackageProperty() {
