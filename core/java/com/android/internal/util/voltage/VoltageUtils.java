01a81abbc26c85ddc728aaf3b6ab1c8701d2efe2
Pranav Vashi <neobuddy89@gmail.com>
2023-02-26 16:57:25 +0530

    AppLock: Allow locking all apps visible in launcher
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- core/java/com/android/internal/util/voltage/VoltageUtils.java
+++ core/java/com/android/internal/util/voltage/VoltageUtils.java
@@ -51,6 +51,7 @@ import android.content.om.OverlayInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.ResolveInfo;
 import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.res.Resources;
 import android.database.ContentObserver;
@@ -258,6 +259,20 @@ public class VoltageUtils {
         } catch (NameNotFoundException e) {
             return false;
         }
+    }
+
+    public static List<String> launchablePackages(Context context) {
+        List<String> list = new ArrayList<>();
+        Intent filter = new Intent(Intent.ACTION_MAIN, null);
+        filter.addCategory(Intent.CATEGORY_LAUNCHER);
+        List<ResolveInfo> apps = context.getPackageManager().queryIntentActivities(filter,
+                PackageManager.GET_META_DATA);
+        int numPackages = apps.size();
+        for (int i = 0; i < numPackages; i++) {
+            ResolveInfo app = apps.get(i);
+            list.add(app.activityInfo.packageName);
+        }
+        return list;
     }
 
     public static boolean deviceSupportsFlashLight(Context context) {
