cb2a4963c99d4bec445e411f48dbf039a179f7b8
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-03-03 08:57:45 +0200

    add GNSS SUPL setting
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -14060,6 +14060,10 @@ public final class Settings {
         @Protected(restrictReads = false, readWrite = KnownSystemPackage.SETTINGS)
         public static final String GEOCODER = "geocoder";
 
+	/** @hide */
+        @Protected(restrictReads = false, readWrite = KnownSystemPackage.SETTINGS)
+        public static final String GNSS_SUPL = "force_disable_supl"; // historical name
+
         // ExtSettings END
 
         // NOTE: If you add new settings here, be sure to add them to
