87e0fdf51c8856bc4775cdb37184993b343f91e2
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-05-16 15:38:31 +0300

    support redirecting ContentProvider calls to another provider
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- core/java/android/app/ContextImpl.java
+++ core/java/android/app/ContextImpl.java
@@ -21,6 +21,7 @@ import static android.content.pm.PackageManager.PERMISSION_GRANTED;
 import static android.os.StrictMode.vmIncorrectContextUseEnabled;
 import static android.permission.flags.Flags.shouldRegisterAttributionSource;
 import static android.view.WindowManager.LayoutParams.WindowType;
+import static com.android.internal.app.ContentProviderRedirector.translateContentProviderAuthority;
 import static com.android.internal.gmscompat.GmcDebug.maybeLogSendBroadcast;
 import static com.android.internal.gmscompat.GmcDebug.maybeLogStartService;
 
@@ -4221,6 +4222,7 @@ class ContextImpl extends Context {
         @UnsupportedAppUsage
         @RavenwoodRedirect
         protected IContentProvider acquireProvider(Context context, String auth) {
+            auth = translateContentProviderAuthority(auth);
             return mMainThread.acquireProvider(context,
                     ContentProvider.getAuthorityWithoutUserId(auth),
                     resolveUserIdFromAuthority(auth), true);
@@ -4229,6 +4231,7 @@ class ContextImpl extends Context {
         @Override
         @RavenwoodRedirect
         protected IContentProvider acquireExistingProvider(Context context, String auth) {
+            auth = translateContentProviderAuthority(auth);
             return mMainThread.acquireExistingProvider(context,
                     ContentProvider.getAuthorityWithoutUserId(auth),
                     resolveUserIdFromAuthority(auth), true);
@@ -4243,6 +4246,7 @@ class ContextImpl extends Context {
         @Override
         @RavenwoodRedirect
         protected IContentProvider acquireUnstableProvider(Context c, String auth) {
+            auth = translateContentProviderAuthority(auth);
             return mMainThread.acquireProvider(c,
                     ContentProvider.getAuthorityWithoutUserId(auth),
                     resolveUserIdFromAuthority(auth), false);
