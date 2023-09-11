d2ad844e6e77ec1b03d48d52f9d118f2224f0dd2
tejasvp25 <tejasvp25@gmail.com>
2023-09-11 15:06:59 +0530

    SystemUI: Allow to exlude apps from running apps notification
    Signed-off-by: tejasvp25 <tejasvp25@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/FgsManagerController.kt
+++ packages/SystemUI/src/com/android/systemui/qs/FgsManagerController.kt
@@ -141,6 +141,7 @@ interface FgsManagerController {
 class FgsManagerControllerImpl
 @Inject
 constructor(
+    private val context: Context,
     @ShadeDisplayAware private val resources: Resources,
     @Main private val mainExecutor: Executor,
     @Background private val backgroundExecutor: Executor,
@@ -232,6 +233,9 @@ constructor(
         resources.getStringArray(com.android.internal.R.array.vendor_stoppable_fgs_system_apps)
     }
 
+    @GuardedBy("lock")
+    private val excludedApps: Array<String> = context.resources.getStringArray(R.array.excluded_apps)
+
     override fun init() {
         synchronized(lock) {
             if (initialized) {
@@ -319,7 +323,6 @@ constructor(
                 executor = mainExecutor,
                 flags = Context.RECEIVER_NOT_EXPORTED,
             )
-
             initialized = true
         }
     }
@@ -659,7 +662,7 @@ constructor(
         ) {
             synchronized(lock) {
                 val userPackageKey = UserPackage(userId, packageName)
-                if (isForeground) {
+                if (isForeground && !excludedApps.contains(packageName)) {
                     runningTaskIdentifiers
                         .getOrPut(userPackageKey) { StartTimeAndIdentifiers(systemClock) }
                         .addFgsToken(token)
@@ -691,7 +694,7 @@ constructor(
                         UserHandle.getUserId(summary.callingUid),
                         summary.callingPackageName,
                     )
-                if (isRunning) {
+                if (isRunning && !excludedApps.contains(summary.callingPackageName)) {
                     runningTaskIdentifiers
                         .getOrPut(userPackageKey) { StartTimeAndIdentifiers(systemClock) }
                         .addJobSummary(summary)
