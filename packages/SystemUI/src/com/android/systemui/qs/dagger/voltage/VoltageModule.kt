6b274adf68e5e08013178ee7556b59da11c96c80
Adnan Begovic <adnan@cyngn.com>
2023-11-09 19:16:16 +0530

    SystemUI: Add AmbientDisplay tile Squashed with:
    Author: Adnan Begovic <adnan@cyngn.com>
    Date:   Fri Dec 11 16:16:02 2015 -0800
    
        SystemUI: Readd AmbientDisplayTile.
    
        Change-Id: Ie7b93ba0f87d505206b5d60f193c2234d83d7eac
    
    Author: Michael Bestas <mkbestas@lineageos.org>
    Date:   Sun Sep 17 22:51:15 2017 +0300
    
        SystemUI: Hide ambient display tile if device does not support it
    
        * Add a check similar to the Settings app
    
        Change-Id: I603cd28b8dd1fadff9ffcfb6953c7b72514b5857
    
    Author: Luca Stefani <luca.stefani.ge1@gmail.com>
    Date:   Wed Mar 17 22:34:17 2021 +0100
    
        SystemUI: AmbientTile: Don't hardcode display settings intent
    
        Change-Id: Ifaa69c2f235b85ae847524c81f0ba59e2a51cf01
    
    Author: Luca Stefani <luca.stefani.ge1@gmail.com>
    Date:   Wed Mar 17 22:37:04 2021 +0100
    
        SystemUI: AmbientTile: Use SecureSetting to change doze setting
    
        Change-Id: I7f004bcf45493130ad50faacb6e390cd640e53db
    
    Author: LuK1337 <priv.luk@gmail.com>
    Date:   Wed Mar 17 22:40:49 2021 +0100
    
        SystemUI: AmbientTile: Actually start using SecureSetting
    
        Change-Id: I5d94f6d2e596dbc6b44ae4f30f479d335a067661
    
    Author: Luca Stefani <luca.stefani.ge1@gmail.com>
    Date:   Thu Mar 18 10:51:54 2021 +0100
    
        SystemUI: AmbientTile: Default the setting to 1
    
        Change-Id: Idea9dda702fdbaa0e2836e7a2573aa57cf24449e
    
    Change-Id: Iea339685f523016876f059052e66e97fd5170a81
    
    [someone5678] Forward port to QPR3
    
    Signed-off-by: someone5678 <nemui3353@gmail.com>
    
    Signed-off-by: Saikrishna1504 <saikrishna26918@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/dagger/voltage/VoltageModule.kt
+++ packages/SystemUI/src/com/android/systemui/qs/dagger/voltage/VoltageModule.kt
@@ -23,6 +23,7 @@ import com.android.systemui.qs.tileimpl.QSTileImpl
 import com.android.systemui.qs.tiles.RefreshRateTile
 import com.android.systemui.qs.tiles.HeadsUpTile
 import com.android.systemui.qs.tiles.SyncTile
+import com.android.systemui.qs.tiles.AmbientDisplayTile
 import com.android.systemui.qs.tiles.base.shared.model.QSTileConfig;
 import com.android.systemui.qs.tiles.base.shared.model.QSTilePolicy;
 import com.android.systemui.qs.tiles.base.shared.model.QSTileUIConfig;
@@ -53,6 +54,12 @@ interface VoltageModule {
     @IntoMap
     @StringKey(SyncTile.TILE_SPEC)
     fun bindSyncTile(syncTile: SyncTile): QSTileImpl<*>
+
+    /** Inject AmbientDisplayTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(AmbientDisplayTile.TILE_SPEC)
+    fun bindAmbientDisplayTile(ambientDisplayTile: AmbientDisplayTile): QSTileImpl<*>
 
     companion object {
         @Provides
@@ -97,5 +104,22 @@ interface VoltageModule {
                 ),
                 instanceId = uiEventLogger.getNewInstanceId(),
                 category = TileCategory.CONNECTIVITY
+            )
+        }
+
+        @Provides
+        @IntoMap
+        @StringKey(AmbientDisplayTile.TILE_SPEC)
+        fun provideAmbientDisplayConfig(uiEventLogger: QsEventLogger): QSTileConfig {
+            return QSTileConfig(
+                tileSpec = TileSpec.create(AmbientDisplayTile.TILE_SPEC),
+                uiConfig = QSTileUIConfig.Resource(
+                    iconRes = R.drawable.ic_qs_ambient_display,
+                    labelRes = R.string.quick_settings_ambient_display_label
+                ),
+                instanceId = uiEventLogger.getNewInstanceId(),
+                category = TileCategory.DISPLAY
+            )
+        }
     }
 }
