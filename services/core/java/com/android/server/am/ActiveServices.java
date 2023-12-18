5f8c87b2f376861ba2019eb1eda3e3ca698be331
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-12-18 19:28:01 +0200

    PackageHooks: infrastructure for per-package hooks in system_server
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- services/core/java/com/android/server/am/ActiveServices.java
+++ services/core/java/com/android/server/am/ActiveServices.java
@@ -8862,6 +8862,11 @@ public final class ActiveServices {
                 ret = REASON_DEVICE_OWNER;
             }
         }
+        if (ret == REASON_DENIED) {
+            if (ActiveServicesHooks.shouldAllowFgsWhileInUsePermission(this, callingUid)) {
+                ret = REASON_ALLOWLISTED_PACKAGE;
+            }
+        }
         return ret;
     }
 
