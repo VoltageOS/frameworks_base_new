5cf64ae1f2d3545996f65071139d399a20a9a04f
raystef66 <s.vanbarel@gmail.com>
2024-02-11 12:32:57 +0100

    SystemUI: Allow limiting AOD & ambient display refresh rate via overlay
    This should save some juice while showing it. AOSP has
     implementation for keyguard & AOD but lack AOD only - We still want to show
     keyguard at max rate to allow better UX
    
    This feature should behave fine with config_keyguardMaxRefreshRate and/or config_keyguardPrefferedRefreshRate set
    and thus allows setting a different rate for each
    
    Suggested-by: John Galt <johngaltfirstrun@gmail.com>
    
    Co-authored-by: Ido Ben-Hur <idoybh2@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- packages/SystemUI/src/com/android/systemui/shade/NotificationShadeWindowControllerImpl.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationShadeWindowControllerImpl.java
@@ -120,6 +120,7 @@ public class NotificationShadeWindowControllerImpl
     private final long mLockScreenDisplayTimeout;
     private final float mKeyguardPreferredRefreshRate; // takes precedence over max
     private final float mKeyguardMaxRefreshRate;
+    private final float mAODMaxRefreshRate;
     private final KeyguardViewMediator mKeyguardViewMediator;
     private final KeyguardBypassController mKeyguardBypassController;
     private final Executor mBackgroundExecutor;
@@ -233,6 +234,12 @@ public class NotificationShadeWindowControllerImpl
         mKeyguardMaxRefreshRate =
                 context.getResources().getInteger(R.integer.config_keyguardMaxRefreshRate);
         mTopUiController = topUiController;
+
+        // Same as described above but limited to AOD
+        // Allows using a different rate for each
+        mAODMaxRefreshRate = context.getResources()
+                .getInteger(R.integer.config_aodMaxRefreshRate);
+
         mKeyguardSurfaceBehindInteractor = keyguardSurfaceBehindInteractor;
 
         if (SceneContainerFlag.isEnabled()) {
@@ -445,6 +452,8 @@ public class NotificationShadeWindowControllerImpl
     private void applyKeyguardFlags(NotificationShadeWindowState state) {
         final boolean keyguardOrAod =
                 state.keyguardShowing || (state.dozing && mDozeParameters.getAlwaysOn());
+        boolean wasKeyguardRateSet = false;
+
         if ((keyguardOrAod && !state.mediaBackdropShowing && !state.lightRevealScrimOpaque)
                 || (!SceneContainerFlag.isEnabled()
                         && mKeyguardViewMediator.isAnimatingBetweenKeyguardAndSurfaceBehind())
@@ -480,6 +489,7 @@ public class NotificationShadeWindowControllerImpl
                 // Both max and min display refresh rate must be set to take effect:
                 mLpChanged.preferredMaxDisplayRefreshRate = mKeyguardPreferredRefreshRate;
                 mLpChanged.preferredMinDisplayRefreshRate = mKeyguardPreferredRefreshRate;
+                wasKeyguardRateSet = true;
             } else {
                 mLpChanged.preferredMaxDisplayRefreshRate = 0;
                 mLpChanged.preferredMinDisplayRefreshRate = 0;
@@ -495,6 +505,7 @@ public class NotificationShadeWindowControllerImpl
                             && !state.keyguardGoingAway;
             if (state.dozing || bypassOnKeyguard) {
                 mLpChanged.preferredMaxDisplayRefreshRate = mKeyguardMaxRefreshRate;
+                wasKeyguardRateSet = true;
             } else {
                 mLpChanged.preferredMaxDisplayRefreshRate = 0;
             }
@@ -512,6 +523,19 @@ public class NotificationShadeWindowControllerImpl
             mLpChanged.inputFeatures |= LayoutParams.INPUT_FEATURE_SENSITIVE_FOR_PRIVACY;
         } else {
             mLpChanged.inputFeatures &= ~LayoutParams.INPUT_FEATURE_SENSITIVE_FOR_PRIVACY;
+        }
+
+        if (mAODMaxRefreshRate > 0) {
+            if (state.dozing) {
+                // limit on AOD & ambient if we have that set
+                // overrides set max keyguard rate
+                mLpChanged.preferredMaxDisplayRefreshRate = mAODMaxRefreshRate;
+            } else if (!wasKeyguardRateSet) {
+                // un-limit when out, but only if max keyguard rate wasn't set
+                mLpChanged.preferredMaxDisplayRefreshRate = 0;
+            }
+            Trace.setCounter("display_max_refresh_rate",
+                        (long) mLpChanged.preferredMaxDisplayRefreshRate);
         }
     }
 
