fcfe02b7280b6c8fe4d85f1d50786332219652f4
Jagdish Prajapati <quic_jprajapa@quicinc.com>
2024-06-26 03:45:46 +0530

    wm: Keep Wallpaper always at the bottom
    Restore wallpaper transition handling as per last android version
    to fix certain unexpected wallpaper transition behavior.
    
    Bug: 349420127
    Change-Id: I1e19740e4a9a1015625907f0b9957da826cb00fe
    CRs-Fixed: 3786167
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- libs/WindowManager/Shell/src/com/android/wm/shell/transition/Transitions.java
+++ libs/WindowManager/Shell/src/com/android/wm/shell/transition/Transitions.java
@@ -37,6 +37,7 @@ import static android.window.TransitionInfo.FLAG_IS_OCCLUDED;
 import static android.window.TransitionInfo.FLAG_IS_WALLPAPER;
 import static android.window.TransitionInfo.FLAG_NO_ANIMATION;
 import static android.window.TransitionInfo.FLAG_STARTING_WINDOW_TRANSFER_RECIPIENT;
+import static android.window.TransitionInfo.FLAG_IS_WALLPAPER;
 
 import static com.android.window.flags.Flags.unifyShellBinders;
 import static com.android.wm.shell.protolog.ShellProtoLogGroup.WM_SHELL_TRANSITIONS;
@@ -628,7 +629,14 @@ public class Transitions implements RemoteCallable<Transitions>,
         final boolean isClosing = isClosingType(transitType);
         final int mode = change.getMode();
         // Put all the OPEN/SHOW on top
-        if (mode == TRANSIT_OPEN || mode == TRANSIT_TO_FRONT) {
+        if ((change.getFlags() & FLAG_IS_WALLPAPER) != 0) {
+            // Wallpaper is always at the bottom, opening wallpaper on top of closing one.
+            if (mode == TRANSIT_OPEN || mode == TRANSIT_TO_FRONT) {
+                return -zSplitLine + numChanges - i;
+            } else {
+                return -zSplitLine - i;
+            }
+        } else if (mode == TRANSIT_OPEN || mode == TRANSIT_TO_FRONT) {
             if (isOpening) {
                 // put on top
                 return zSplitLine + numChanges - i;
