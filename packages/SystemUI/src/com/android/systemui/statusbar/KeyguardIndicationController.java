2a53290d6d1bf6da352b423cc80127eb4795c917
Aston-Martinn <advaithbhat9@gmail.com>
2024-09-07 10:54:23 +0530

    SystemUI: !fixup Ambient display show battery & cpu temperature
    Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
@@ -1237,7 +1237,7 @@ public class KeyguardIndicationController {
             } else {
                 String batteryLevel = NumberFormat.getPercentInstance().format(mBatteryLevel / 100f);
                 String batteryTemp = com.android.internal.util.voltage.VoltageUtils.batteryTemperature(mContext, false);
-                String cpuTemp = com.android.internal.util.voltage.VoltageUtils.batteryTemperature(mContext, false);
+                String cpuTemp = com.android.internal.util.voltage.VoltageUtils.getCPUTemp(mContext);
 
                 Drawable batteryIcon = mContext.getDrawable(R.drawable.ic_ambient_battery);
                 Drawable cpuIcon = mContext.getDrawable(R.drawable.ic_ambient_cpu);
