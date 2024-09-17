d4da5f397be99b2a6e7f4043693cb24b07ad1d5e
maxwen <max.weninger@gmail.com>
2024-09-17 13:31:07 +0530

    SystemUI: [SQUASH] Keyguard battery bar [1/2]
    Squashed-in:
    * SystemUI: keyguard battery bar in aod on charging [1/2]
      or always with an switch (maxwen)
    [micky387] Remove tuner dependency and add settings for our OmniGears
    
    [spezi77] Updated for A13 & A14
    * SystemUI: Also show the battery bar if the battery is not charging.
      Only show it while in doze mode (Ambient/AoD) (spezi77)
    
    [Albinoman887] Fix USB plug/unplug on AOD when battery bar setting is disabled
    * This check was added after the following commit that intruduces the Keyguard Battery Bar
      Setting was authored. Simply bring back the check and bail out of the function if it isn't
      visible.
    
    Co-authored-by: spezi77 <spezi7713@gmx.net>
    Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
@@ -129,6 +129,7 @@ import com.android.systemui.statusbar.phone.KeyguardIndicationTextView;
 import com.android.systemui.statusbar.phone.StatusBarKeyguardViewManager;
 import com.android.systemui.statusbar.policy.KeyguardStateController;
 import com.android.systemui.user.domain.interactor.UserLogoutInteractor;
+import com.android.systemui.voltage.BatteryBarView;
 import com.android.systemui.util.AlarmTimeout;
 import com.android.systemui.util.concurrency.DelayableExecutor;
 import com.android.systemui.util.wakelock.SettableWakeLock;
@@ -258,6 +259,8 @@ public class KeyguardIndicationController {
 
     private IBatteryPropertiesRegistrar mBatteryPropertiesRegistrar;
     private boolean mAlternateFastchargeInfoUpdate;
+
+    private BatteryBarView mBatteryBar;
 
     private KeyguardUpdateMonitorCallback mUpdateMonitorCallback;
 
@@ -502,6 +505,7 @@ public class KeyguardIndicationController {
                 mStatusBarStateController,
                 mKeyguardLogger
         );
+        mBatteryBar = indicationArea.findViewById(R.id.battery_bar_view);
         updateDeviceEntryIndication(false /* animate */);
         updateOrganizedOwnedDevice();
         if (mBroadcastReceiver == null) {
@@ -1190,12 +1194,20 @@ public class KeyguardIndicationController {
             return;
         }
 
+        boolean showBatteryBar = Settings.System.getIntForUser(mContext.getContentResolver(),
+                 Settings.System.CUSTOM_KEYGUARD_SHOW_BATTERY_BAR, 0, UserHandle.USER_CURRENT) == 1;
+        boolean showBatteryBarAlways = Settings.System.getIntForUser(mContext.getContentResolver(),
+                 Settings.System.CUSTOM_KEYGUARD_SHOW_BATTERY_BAR_ALWAYS, 0, UserHandle.USER_CURRENT) == 1;
         // A few places might need to hide the indication, so always start by making it visible
         mIndicationArea.setVisibility(VISIBLE);
 
         // Walk down a precedence-ordered list of what indication
         // should be shown based on device state
         if (mDozing) {
+            if (mBatteryBar != null) {
+                mBatteryBar.setVisibility(View.GONE);
+            }
+
             boolean useMisalignmentColor = false;
             mLockScreenIndicationView.setVisibility(View.GONE);
             mTopIndicationView.setVisibility(VISIBLE);
@@ -1217,6 +1229,11 @@ public class KeyguardIndicationController {
                 return;
             } else if (mPowerPluggedIn || mEnableBatteryDefender) {
                 newIndication = computePowerIndication();
+                if (mBatteryBar != null && (showBatteryBar || showBatteryBarAlways)) {
+                    mBatteryBar.setVisibility(View.VISIBLE);
+                    mBatteryBar.setBatteryPercent(mBatteryLevel);
+                    mBatteryBar.setBarColor(Color.WHITE);
+                }
             } else {
                 String batteryTemp = com.android.internal.util.voltage.VoltageUtils.batteryTemperature(mContext, false);
                 String cpuTemp = com.android.internal.util.voltage.VoltageUtils.batteryTemperature(mContext, false);
@@ -1301,7 +1318,13 @@ public class KeyguardIndicationController {
                         newIndication = "";
                         break;
                 }
-            }
+
+                if (mBatteryBar != null && showBatteryBarAlways) {
+                    mBatteryBar.setVisibility(View.VISIBLE);
+                    mBatteryBar.setBatteryPercent(mBatteryLevel);
+                    mBatteryBar.setBarColor(Color.WHITE);
+                }
+             }
 
             if (!TextUtils.equals(mTopIndicationView.getText(), newIndication)) {
                 mWakeLock.setAcquired(true);
