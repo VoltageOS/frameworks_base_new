33783d715b61f0d94e2c2f9baed3f6137fa90dff
Aston-Martinn <advaithbhat9@gmail.com>
2024-09-23 09:51:02 +0530

    SystemUI: Add a gap between ambient icons & battery bar
    Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
@@ -268,6 +268,7 @@ public class KeyguardIndicationController {
     private boolean mAlternateFastchargeInfoUpdate;
 
     private BatteryBarView mBatteryBar;
+    private View mTransparentView;
 
     private KeyguardUpdateMonitorCallback mUpdateMonitorCallback;
 
@@ -516,6 +517,7 @@ public class KeyguardIndicationController {
                 mStatusBarStateController,
                 mKeyguardLogger
         );
+        mTransparentView = indicationArea.findViewById(R.id.transparent_keyguard_view);
         mBatteryBar = indicationArea.findViewById(R.id.battery_bar_view);
         updateDeviceEntryIndication(false /* animate */);
         updateOrganizedOwnedDevice();
@@ -1276,6 +1278,9 @@ public class KeyguardIndicationController {
             if (mBatteryBar != null) {
                 mBatteryBar.setVisibility(View.GONE);
             }
+            if (mTransparentView != null) {
+               mTransparentView.setVisibility(View.GONE);
+            }
 
             boolean useMisalignmentColor = false;
             mLockScreenIndicationView.setVisibility(View.GONE);
@@ -1302,7 +1307,14 @@ public class KeyguardIndicationController {
                     mBatteryBar.setVisibility(View.VISIBLE);
                     mBatteryBar.setBatteryPercent(mBatteryLevel);
                     mBatteryBar.setBarColor(Color.WHITE);
-                }
+                    if (mTransparentView != null) {
+                        if (ambientShowSettingsIcon()) {
+                            mTransparentView.setVisibility(View.VISIBLE);
+                        } else {
+                            mTransparentView.setVisibility(View.GONE);
+                        }
+                    }
+                 }
             } else {
                 String batteryLevel = NumberFormat.getPercentInstance().format(mBatteryLevel / 100f);
                 String batteryTemp = com.android.internal.util.voltage.VoltageUtils.batteryTemperature(mContext, false);
@@ -1358,6 +1370,13 @@ public class KeyguardIndicationController {
                     mBatteryBar.setVisibility(View.VISIBLE);
                     mBatteryBar.setBatteryPercent(mBatteryLevel);
                     mBatteryBar.setBarColor(Color.WHITE);
+                    if (mTransparentView != null) {
+                        if (ambientShowSettingsIcon()) {
+                            mTransparentView.setVisibility(View.VISIBLE);
+                        } else {
+                            mTransparentView.setVisibility(View.GONE);
+                        }
+                    }
                 }
              }
 
