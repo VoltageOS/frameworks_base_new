6b6c3d7e02bfcc0c316c54d9bfe292345351b91a
Ido Ben-Hur <idoybh2@gmail.com>
2024-10-30 17:23:19 +0200

    KeyguardClockSwitchController: Add null checks for weather views
    this crashes when it tries to display the keyguard on a remote / USB display
    it does not display weather there and hence the views are null.
    courtesy of out custom in-place weather view replacement done for omnijaws
    
    Log:
    E/AndroidRuntime: FATAL EXCEPTION: main
    E/AndroidRuntime: Process: com.android.systemui, PID: 11838
    E/AndroidRuntime: java.lang.NullPointerException: Attempt to invoke virtual method 'void android.view.ViewGroup.addView(android.view.View, int, android.view.ViewGroup$LayoutParams)' on a null object reference
    E/AndroidRuntime: 	at com.android.keyguard.KeyguardClockSwitchController.updateViews(go/retraceme 588009ca90c07af19a6ff089934e18a1082b355fe27a9cfa6022f318b62d44a2:117)
    E/AndroidRuntime: 	at com.android.keyguard.KeyguardClockSwitchController.updateWeatherView(go/retraceme 588009ca90c07af19a6ff089934e18a1082b355fe27a9cfa6022f318b62d44a2:30)
    E/AndroidRuntime: 	at com.android.keyguard.KeyguardStatusViewController$4.onKeyguardVisibilityChanged(go/retraceme 588009ca90c07af19a6ff089934e18a1082b355fe27a9cfa6022f318b62d44a2:12)
    E/AndroidRuntime: 	at com.android.keyguard.KeyguardUpdateMonitor.registerCallback(go/retraceme 588009ca90c07af19a6ff089934e18a1082b355fe27a9cfa6022f318b62d44a2:82)
    E/AndroidRuntime: 	at com.android.keyguard.KeyguardStatusViewController.onViewAttached(go/retraceme 588009ca90c07af19a6ff089934e18a1082b355fe27a9cfa6022f318b62d44a2:23)
    ...
    
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- packages/SystemUI/src/com/android/keyguard/KeyguardClockSwitchController.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardClockSwitchController.java
@@ -434,6 +434,9 @@ public class KeyguardClockSwitchController extends ViewController<KeyguardClockS
         mDateWeatherView = (ViewGroup) mSmartspaceController.buildAndConnectDateView(mView);
         LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
                 MATCH_PARENT, WRAP_CONTENT);
+        if (mDateWeatherView == null || mStatusArea == null) {
+            return;
+        }
         mStatusArea.addView(mDateWeatherView, 0, lp);
         int startPadding = getContext().getResources().getDimensionPixelSize(
                 R.dimen.below_clock_padding_start);
@@ -450,6 +453,9 @@ public class KeyguardClockSwitchController extends ViewController<KeyguardClockS
         LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
                 WRAP_CONTENT, WRAP_CONTENT);
         mWeatherView = mSmartspaceController.buildAndConnectWeatherView(mView);
+        if (mWeatherView == null) {
+            return;
+        }
         // Place weather right after the date, before the extras
         final int index = mDateWeatherView.getChildCount() == 0 ? 0 : 1;
         mDateWeatherView.addView(mWeatherView, index, lp);
@@ -468,6 +474,9 @@ public class KeyguardClockSwitchController extends ViewController<KeyguardClockS
         mSmartspaceView = mSmartspaceController.buildAndConnectView(mView);
         LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
                 MATCH_PARENT, WRAP_CONTENT);
+        if (mStatusArea == null || mSmartspaceView == null) {
+            return;
+        }
         mStatusArea.addView(mSmartspaceView, 0, lp);
         int startPadding = getContext().getResources().getDimensionPixelSize(
                 R.dimen.below_clock_padding_start);
