798e40ddf16805bb6073544909ca757f8927b901
Lukasz Merta <lukasz@haleytek.com>
2024-11-21 11:17:18 +0100

    Deregistering mixes before register mixes in connectMixes
    When AudioFlinger binder dies and AudioService receives
    onError/onAudioServerDied callback but AudioPolicy service is alive,
    we get INVALID_OPERATION error because mixes was already registered.
    This causes calling release on AudioPolicyProxy.
    
    Test: Manual
    Bug: 380092408
    Google: 3370477
    Change-Id: I4e5c6e3d3aace03f02f312fda26e5696ac090d04
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- services/core/java/com/android/server/audio/AudioService.java
+++ services/core/java/com/android/server/audio/AudioService.java
@@ -14662,6 +14662,7 @@ public class AudioService extends IAudioService.Stub
                 for (AudioMix mix : mMixes) {
                     mix.setVirtualDeviceId(mAttributionSource.getDeviceId());
                 }
+                mAudioSystem.registerPolicyMixes(mMixes, false);
                 return mAudioSystem.registerPolicyMixes(mMixes, true);
             } finally {
                 Binder.restoreCallingIdentity(identity);
