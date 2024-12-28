bezke/frameworks_base_derp

base: Implement Lineage health service
Change-Id: I772ccf6d323c24d681aa8468bf4318c7b73bd3f5
Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>

core/java/com/android/internal/derp/health/IHealthInterface.aidl
@@ -1,0 +1,40 @@
+/**
+ * Copyright (c) 2023 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.internal.derp.health;
+
+/** @hide */
+interface IHealthInterface {
+    boolean isChargingControlSupported();
+
+    boolean getChargingControlEnabled();
+    boolean setChargingControlEnabled(boolean enabled);
+
+    int getChargingControlMode();
+    boolean setChargingControlMode(int mode);
+
+    int getChargingControlStartTime();
+    boolean setChargingControlStartTime(int time);
+
+    int getChargingControlTargetTime();
+    boolean setChargingControlTargetTime(int time);
+
+    int getChargingControlLimit();
+    boolean setChargingControlLimit(int limit);
+
+    boolean resetChargingControl();
+    boolean allowFineGrainedSettings();
+}

services/core/java/com/android/server/derp/health/HealthInterfaceService.java
@@ -1,0 +1,164 @@
+/*
+ * Copyright (C) 2023 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.server.derp.health;
+
+import android.Manifest;
+import android.content.Context;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Process;
+import android.util.Log;
+
+import com.android.server.ServiceThread;
+
+import com.android.server.SystemService;
+
+import com.android.internal.derp.app.LineageContextConstants;
+import com.android.internal.derp.health.IHealthInterface;
+import vendor.lineage.health.ChargingControlSupportedMode;
+
+import java.io.FileDescriptor;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+public class HealthInterfaceService extends SystemService {
+
+    private static final String TAG = "LineageHealth";
+    private final Context mContext;
+    private final Handler mHandler;
+    private final ServiceThread mHandlerThread;
+
+    private final List<LineageHealthFeature> mFeatures = new ArrayList<LineageHealthFeature>();
+
+    // Health features
+    private ChargingControlController mCCC;
+
+    public HealthInterfaceService(Context context) {
+        super(context);
+        mContext = context;
+
+        mHandlerThread = new ServiceThread(TAG, Process.THREAD_PRIORITY_DEFAULT, false);
+        mHandlerThread.start();
+        mHandler = new Handler(mHandlerThread.getLooper());
+    }
+
+    @Override
+    public void onStart() {
+        mCCC = new ChargingControlController(mContext, mHandler);
+        if (mCCC.isSupported()) {
+            mFeatures.add(mCCC);
+        }
+
+        if (!mFeatures.isEmpty()) {
+            publishBinderService(LineageContextConstants.LINEAGE_HEALTH_INTERFACE, mService);
+        }
+    }
+
+    @Override
+    public void onBootPhase(int phase) {
+        if (phase != PHASE_BOOT_COMPLETED) {
+            return;
+        }
+
+        // start and update all features
+        for (LineageHealthFeature feature : mFeatures) {
+            feature.start();
+        }
+    }
+
+    /* Service */
+    private final IBinder mService = new IHealthInterface.Stub() {
+        @Override
+        public boolean isChargingControlSupported() {
+            return mCCC.isSupported();
+        }
+
+        @Override
+        public boolean getChargingControlEnabled() {
+            return mCCC.isEnabled();
+        }
+
+        @Override
+        public boolean setChargingControlEnabled(boolean enabled) {
+            return mCCC.setEnabled(enabled);
+        }
+
+        @Override
+        public int getChargingControlMode() {
+            return mCCC.getMode();
+        }
+
+        @Override
+        public boolean setChargingControlMode(int mode) {
+            return mCCC.setMode(mode);
+        }
+
+        @Override
+        public int getChargingControlStartTime() {
+            return mCCC.getStartTime();
+        }
+
+        @Override
+        public boolean setChargingControlStartTime(int startTime) {
+            return mCCC.setStartTime(startTime);
+        }
+
+        @Override
+        public int getChargingControlTargetTime() {
+            return mCCC.getTargetTime();
+        }
+
+        @Override
+        public boolean setChargingControlTargetTime(int targetTime) {
+            return mCCC.setTargetTime(targetTime);
+        }
+
+        @Override
+        public int getChargingControlLimit() {
+            return mCCC.getLimit();
+        }
+
+        @Override
+        public boolean setChargingControlLimit(int limit) {
+            return mCCC.setLimit(limit);
+        }
+
+        @Override
+        public boolean resetChargingControl() {
+            return mCCC.reset();
+        }
+
+        @Override
+        public boolean allowFineGrainedSettings() {
+            // We allow fine-grained settings if allow toggle and bypass
+            return mCCC.isChargingModeSupported(ChargingControlSupportedMode.TOGGLE);
+        }
+
+        @Override
+        public void dump(FileDescriptor fd, PrintWriter pw, String[] args) {
+            mContext.enforceCallingOrSelfPermission(Manifest.permission.DUMP, TAG);
+
+            pw.println();
+            pw.println("LineageHealth Service State:");
+
+            for (LineageHealthFeature feature : mFeatures) {
+                feature.dump(pw);
+            }
+        }
+    };
+}

services/core/java/com/android/server/derp/health/LineageHealthFeature.java
@@ -1,0 +1,32 @@
+/*
+ * Copyright (C) 2023 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.server.derp.health;
+
+import android.content.Context;
+import android.os.Handler;
+
+import com.android.server.derp.LineageBaseFeature;
+
+public abstract class LineageHealthFeature extends LineageBaseFeature {
+    protected static final String TAG = "LineageHealth";
+
+    public LineageHealthFeature(Context context, Handler handler) {
+        super(context, handler);
+    }
+
+    public abstract boolean isSupported();
+}

VoltageOS/frameworks_base_new

Signed-off-by: Dmitrii <bankersenator@gmail.com>
Signed-off-by: Kanishk <kanishkthederp@gmail.com>

core/java/com/android/internal/custom/health/IHealthInterface.aidl
@@ -1,0 +1,40 @@
+/**
+ * Copyright (c) 2023 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.internal.custom.health;
+
+/** @hide */
+interface IHealthInterface {
+    boolean isChargingControlSupported();
+
+    boolean getChargingControlEnabled();
+    boolean setChargingControlEnabled(boolean enabled);
+
+    int getChargingControlMode();
+    boolean setChargingControlMode(int mode);
+
+    int getChargingControlStartTime();
+    boolean setChargingControlStartTime(int time);
+
+    int getChargingControlTargetTime();
+    boolean setChargingControlTargetTime(int time);
+
+    int getChargingControlLimit();
+    boolean setChargingControlLimit(int limit);
+
+    boolean resetChargingControl();
+    boolean allowFineGrainedSettings();
+}

services/core/java/com/android/server/custom/health/HealthInterfaceService.java
@@ -1,0 +1,164 @@
+/*
+ * Copyright (C) 2023 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.server.custom.health;
+
+import android.Manifest;
+import android.content.Context;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Process;
+import android.util.Log;
+
+import com.android.server.ServiceThread;
+
+import com.android.server.SystemService;
+
+import com.android.internal.custom.app.LineageContextConstants;
+import com.android.internal.custom.health.IHealthInterface;
+import vendor.lineage.health.ChargingControlSupportedMode;
+
+import java.io.FileDescriptor;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+public class HealthInterfaceService extends SystemService {
+
+    private static final String TAG = "LineageHealth";
+    private final Context mContext;
+    private final Handler mHandler;
+    private final ServiceThread mHandlerThread;
+
+    private final List<LineageHealthFeature> mFeatures = new ArrayList<LineageHealthFeature>();
+
+    // Health features
+    private ChargingControlController mCCC;
+
+    public HealthInterfaceService(Context context) {
+        super(context);
+        mContext = context;
+
+        mHandlerThread = new ServiceThread(TAG, Process.THREAD_PRIORITY_DEFAULT, false);
+        mHandlerThread.start();
+        mHandler = new Handler(mHandlerThread.getLooper());
+    }
+
+    @Override
+    public void onStart() {
+        mCCC = new ChargingControlController(mContext, mHandler);
+        if (mCCC.isSupported()) {
+            mFeatures.add(mCCC);
+        }
+
+        if (!mFeatures.isEmpty()) {
+            publishBinderService(LineageContextConstants.LINEAGE_HEALTH_INTERFACE, mService);
+        }
+    }
+
+    @Override
+    public void onBootPhase(int phase) {
+        if (phase != PHASE_BOOT_COMPLETED) {
+            return;
+        }
+
+        // start and update all features
+        for (LineageHealthFeature feature : mFeatures) {
+            feature.start();
+        }
+    }
+
+    /* Service */
+    private final IBinder mService = new IHealthInterface.Stub() {
+        @Override
+        public boolean isChargingControlSupported() {
+            return mCCC.isSupported();
+        }
+
+        @Override
+        public boolean getChargingControlEnabled() {
+            return mCCC.isEnabled();
+        }
+
+        @Override
+        public boolean setChargingControlEnabled(boolean enabled) {
+            return mCCC.setEnabled(enabled);
+        }
+
+        @Override
+        public int getChargingControlMode() {
+            return mCCC.getMode();
+        }
+
+        @Override
+        public boolean setChargingControlMode(int mode) {
+            return mCCC.setMode(mode);
+        }
+
+        @Override
+        public int getChargingControlStartTime() {
+            return mCCC.getStartTime();
+        }
+
+        @Override
+        public boolean setChargingControlStartTime(int startTime) {
+            return mCCC.setStartTime(startTime);
+        }
+
+        @Override
+        public int getChargingControlTargetTime() {
+            return mCCC.getTargetTime();
+        }
+
+        @Override
+        public boolean setChargingControlTargetTime(int targetTime) {
+            return mCCC.setTargetTime(targetTime);
+        }
+
+        @Override
+        public int getChargingControlLimit() {
+            return mCCC.getLimit();
+        }
+
+        @Override
+        public boolean setChargingControlLimit(int limit) {
+            return mCCC.setLimit(limit);
+        }
+
+        @Override
+        public boolean resetChargingControl() {
+            return mCCC.reset();
+        }
+
+        @Override
+        public boolean allowFineGrainedSettings() {
+            // We allow fine-grained settings if allow toggle and bypass
+            return mCCC.isChargingModeSupported(ChargingControlSupportedMode.TOGGLE);
+        }
+
+        @Override
+        public void dump(FileDescriptor fd, PrintWriter pw, String[] args) {
+            mContext.enforceCallingOrSelfPermission(Manifest.permission.DUMP, TAG);
+
+            pw.println();
+            pw.println("LineageHealth Service State:");
+
+            for (LineageHealthFeature feature : mFeatures) {
+                feature.dump(pw);
+            }
+        }
+    };
+}

services/core/java/com/android/server/custom/health/LineageHealthFeature.java
@@ -1,0 +1,32 @@
+/*
+ * Copyright (C) 2023 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.server.custom.health;
+
+import android.content.Context;
+import android.os.Handler;
+
+import com.android.server.custom.LineageBaseFeature;
+
+public abstract class LineageHealthFeature extends LineageBaseFeature {
+    protected static final String TAG = "LineageHealth";
+
+    public LineageHealthFeature(Context context, Handler handler) {
+        super(context, handler);
+    }
+
+    public abstract boolean isSupported();
+}