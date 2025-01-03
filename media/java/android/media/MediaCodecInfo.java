bionic-linux/android_frameworks_base

Java and JNI support for native PerformancePoint.
Bug: b/306023029
Test: build
Change-Id: Ie0864e21923fee4a4d72cfb8251ca78c37bca363

media/java/android/media/MediaCodecInfo.java
@@ -2227,6 +2227,26 @@ public final class MediaCodecInfo {
             }
 
             /**
+             * Width in macroblocks.
+             *
+             * @hide
+             */
+            @TestApi
+            public int getWidth() {
+                return mWidth;
+            }
+
+            /**
+             * Height in macroblocks.
+             *
+             * @hide
+             */
+            @TestApi
+            public int getHeight() {
+                return mHeight;
+            }
+
+            /**
              * Maximum frame rate in frames per second.
              *
              * @hide
@@ -2246,20 +2266,46 @@ public final class MediaCodecInfo {
                 return mMaxMacroBlockRate;
             }
 
+            /**
+             * Codec block width in macroblocks.
+             *
+             * @hide
+             */
+            @TestApi
+            public int getBlockWidth() {
+                return mBlockSize.getWidth();
+            }
+
+            /**
+             * Codec block height in macroblocks.
+             *
+             * @hide
+             */
+            @TestApi
+            public int getBlockHeight() {
+                return mBlockSize.getHeight();
+            }
+
             /** Convert to a debug string */
             public String toString() {
-                int blockWidth = 16 * mBlockSize.getWidth();
-                int blockHeight = 16 * mBlockSize.getHeight();
-                int origRate = (int)Utils.divUp(mMaxMacroBlockRate, getMaxMacroBlocks());
-                String info = (mWidth * 16) + "x" + (mHeight * 16) + "@" + origRate;
-                if (origRate < mMaxFrameRate) {
-                    info += ", max " + mMaxFrameRate + "fps";
+                if (FLAG) {
+                    return native_toStirng();
+                } else {
+                    int blockWidth = 16 * mBlockSize.getWidth();
+                    int blockHeight = 16 * mBlockSize.getHeight();
+                    int origRate = (int) Utils.divUp(mMaxMacroBlockRate, getMaxMacroBlocks());
+                    String info = (mWidth * 16) + "x" + (mHeight * 16) + "@" + origRate;
+                    if (origRate < mMaxFrameRate) {
+                        info += ", max " + mMaxFrameRate + "fps";
+                    }
+                    if (blockWidth > 16 || blockHeight > 16) {
+                        info += ", " + blockWidth + "x" + blockHeight + " blocks";
+                    }
+                    return "PerformancePoint(" + info + ")";
                 }
-                if (blockWidth > 16 || blockHeight > 16) {
-                    info += ", " + blockWidth + "x" + blockHeight + " blocks";
-                }
-                return "PerformancePoint(" + info + ")";
             }
+
+            static native String native_toString();
 
             @Override
             public int hashCode() {
@@ -2333,6 +2379,15 @@ public final class MediaCodecInfo {
                 this(width, height, frameRate, frameRate /* maxFrameRate */, new Size(16, 16));
             }
 
+            /* package private */ PerformancePoint(int width, int height, int maxFrameRate,
+                    long maxMacroBlockRate, int blockSizeWidth, int blockSizeHeight) {
+                mWidth = width;
+                mHeight = height;
+                mMaxFrameRate = maxFrameRate;
+                mMaxMacroBlockRate = maxMacroBlockRate;
+                mBlockSize = new Size(blockSizeWidth, blockSizeHeight);
+            }
+
             /** Saturates a long value to int */
             private int saturateLongToInt(long value) {
                 if (value < Integer.MIN_VALUE) {
@@ -2386,15 +2441,21 @@ public final class MediaCodecInfo {
              * @return {@code true} if the performance point covers the other.
              */
             public boolean covers(@NonNull PerformancePoint other) {
-                // convert performance points to common block size
-                Size commonSize = getCommonBlockSize(other);
-                PerformancePoint aligned = new PerformancePoint(this, commonSize);
-                PerformancePoint otherAligned = new PerformancePoint(other, commonSize);
+                if (FLAG) {
+                    return native_covers(others);
+                } else {
+                    // convert performance points to common block size
+                    Size commonSize = getCommonBlockSize(other);
+                    PerformancePoint aligned = new PerformancePoint(this, commonSize);
+                    PerformancePoint otherAligned = new PerformancePoint(other, commonSize);
 
-                return (aligned.getMaxMacroBlocks() >= otherAligned.getMaxMacroBlocks()
-                        && aligned.mMaxFrameRate >= otherAligned.mMaxFrameRate
-                        && aligned.mMaxMacroBlockRate >= otherAligned.mMaxMacroBlockRate);
+                    return (aligned.getMaxMacroBlocks() >= otherAligned.getMaxMacroBlocks()
+                            && aligned.mMaxFrameRate >= otherAligned.mMaxFrameRate
+                            && aligned.mMaxMacroBlockRate >= otherAligned.mMaxMacroBlockRate);
+                }
             }
+
+            static native boolean native_covers(PerformancePoint other);
 
             private @NonNull Size getCommonBlockSize(@NonNull PerformancePoint other) {
                 return new Size(
@@ -2407,16 +2468,22 @@ public final class MediaCodecInfo {
                 if (o instanceof PerformancePoint) {
                     // convert performance points to common block size
                     PerformancePoint other = (PerformancePoint)o;
-                    Size commonSize = getCommonBlockSize(other);
-                    PerformancePoint aligned = new PerformancePoint(this, commonSize);
-                    PerformancePoint otherAligned = new PerformancePoint(other, commonSize);
+                    if (FLAG) {
+                        return native_equals(other);
+                    } else {
+                        Size commonSize = getCommonBlockSize(other);
+                        PerformancePoint aligned = new PerformancePoint(this, commonSize);
+                        PerformancePoint otherAligned = new PerformancePoint(other, commonSize);
 
-                    return (aligned.getMaxMacroBlocks() == otherAligned.getMaxMacroBlocks()
-                            && aligned.mMaxFrameRate == otherAligned.mMaxFrameRate
-                            && aligned.mMaxMacroBlockRate == otherAligned.mMaxMacroBlockRate);
+                        return (aligned.getMaxMacroBlocks() == otherAligned.getMaxMacroBlocks()
+                                && aligned.mMaxFrameRate == otherAligned.mMaxFrameRate
+                                && aligned.mMaxMacroBlockRate == otherAligned.mMaxMacroBlockRate);
+                    }
                 }
                 return false;
             }
+
+            static native boolean native_equals(PerformancePoint other);
 
             /** 480p 24fps */
             @NonNull

media/jni/android_media_CodecCapabilities.cpp
@@ -63,6 +63,12 @@ static std::shared_ptr<AudioCapabilities> getAudioCapabilities(JNIEnv *env, jobj
     return std::shared_ptr<AudioCapabilities>(p);
 }
 
+static VideoCapabilities::PerformancePoint& getPerformancePoints(JNIEnv *env, jobject thiz) {
+    VideoCapabilities::PerformancePoint* const p
+            = (VideoCapabilities::PerformancePoint*)env->GetLongField(thiz, fields.context);
+    return *p;
+}
+
 // The Java AudioCapabilities object keep bitrateRange, sampleRates, sampleRateRanges
 // and inputChannelRanges in it to prevent reconstruction when called the getters functions.
 jobject getJavaAudioCapabilitiesFromNative(
@@ -120,6 +126,25 @@ jobject getJavaAudioCapabilitiesFromNative(
     return jAudioCaps;
 }
 
+jobjectArray getJavaPerformancePointArrayFromNative(JNIEnv *env,
+        const std::vector<VideoCapabilities::PerformancePoint>& performancePoints) {
+    jclass performancePointClazz = env->FindClass("android/media/MediaCodecInfo$VideoCapabilities$PerformancePoint");
+    CHECK(performancePointClazz != NULL);
+    jmethodID performancePointConstructID = env->GetMethodID(performancePointClazz, "<init>", "(I;I;I;J;I;I)V");
+    jobjectArray jPerformancePoints = env->NewObjectArray(performancePoints.size(), performancePointClazz, NULL);
+    for (int i = 0; i < performancePoints.size(); i++) {
+        VideoCapabilities::PerformancePoint performancePoint = performancePoints.at(i);
+        jobject jPerformancePoint = env->NewObject(performancePointClazz, performancePointConstructID,
+                performancePoint.getWidth(), performancePoint.getHeight(), performancePoint.getMaxFrameRate(),
+                performancePoint.getMaxMacroBlockRate(), performancePoint.getBlockSize().getWidth(),
+                performancePoint.getBlockSize().getHeight());
+        env->SetObjectArrayElement(jPerformancePoints, i, jPerformancePoint);
+        env->DeleteLocalRef(jPerformancePoint);
+        jPerformancePoint = NULL;
+    }
+    return jPerformancePoints;
+}
+
 // ----------------------------------------------------------------------------
 
 static jint android_media_AudioCapabilities_getMaxInputChannelCount(JNIEnv *env, jobject thiz) {
@@ -155,6 +180,69 @@ static jboolean android_media_AudioCapabilities_isSampleRateSupported(JNIEnv *en
     return res;
 }
 
+// PerformancePoint
+
+static VideoCapabilities::PerformancePoint GetNativePerfromancePointFromJava(JNIEnv *env, jobject pp) {
+    jclass clazz = env->FindClass("android/media/MediaCodecInfo$VideoCapabilities$PerformancePoint");
+    CHECK(clazz != NULL);
+    if (!env->IsInstanceOf(pp, clazz)) {
+        return NULL;
+    }
+
+    jmethodID getWidthID = env->GetMethodID(clazz, "getWidth", "()I");
+    CHECK(getWidthID != NULL);
+    jint width = env->CallIntMethod(pp, getWidthID);
+
+    jmethodID getHeightID = env->GetMethodID(clazz, "getHeight", "()I");
+    CHECK(getHeightID != NULL);
+    jint height = env->CallIntMethod(pp, getHeightID);
+
+    jmethodID getMaxFrameRateID = env->GetMethodID(clazz, "getMaxFrameRate", "()I");
+    CHECK(getMaxFrameRateID != NULL);
+    jint maxFrameRate = env->CallIntMethod(pp, getMaxFrameRateID);
+
+    jmethodID getMaxMacroBlockRateID = env->GetMethodID(clazz, "getMaxMacroBlockRate", "()J");
+    CHECK(getMaxMacroBlockRateID != NULL);
+    jlong maxMacroBlockRate = env->CallLongMethod(pp, getMaxMacroBlockRateID);
+
+    jmethodID getBlockWidthID = env->GetMethodID(clazz, "getBlockWidth", "()I");
+    CHECK(getBlockWidthID != NULL);
+    jint blockWidth = env->CallIntMethod(pp, getBlockWidthID);
+
+    jmethodID getBlockHeightID = env->GetMethodID(clazz, "getBlockHeight", "()I");
+    CHECK(getBlockHeightID != NULL);
+    jint bockHeight = env->CallIntMethod(pp, getBlockHeightID);
+
+    VideoCapabilities::PerformancePoint res = VideoCapabilities::PerformancePoint(width, height, maxFrameRate,
+            maxMacroBlockRate, VideoSize(blockWidth, blockHeight));
+
+    return res;
+}
+
+static jboolean android_media_VideoCapabilities_PerformancePoint_covers(JNIEnv *env, jobject thiz, jobject other) {
+    VideoCapabilities::PerformancePoint& pp0 = getPerformancePoints(env, thiz);
+    VideoCapabilities::PerformancePoint pp1 = GetNativePerfromancePointFromJava(env, other);
+
+    bool res = pp0.covers(pp1);
+
+    return res;
+}
+
+static jboolean android_media_VideoCapabilities_PerformancePoint_equals(JNIEnv *env, jobject thiz, jobject other) {
+    VideoCapabilities::PerformancePoint& pp0 = getPerformancePoints(env, thiz);
+    VideoCapabilities::PerformancePoint pp1 = GetNativePerfromancePointFromJava(env, other);
+
+    bool res = pp0.equals(pp1);
+
+    return res;
+}
+
+static jstring android_media_VideoCapabilities_PerformancePoint_toString(JNIEnv *env, jobject thiz) {
+    VideoCapabilities::PerformancePoint& pp = getPerformancePoints(env, thiz);
+    std::string str = pp.toString();
+    return env->NewStringUTF(str.c_str());
+}
+
 // ----------------------------------------------------------------------------
 
 static const JNINativeMethod gAudioCapsMethods[] = {
@@ -163,9 +251,21 @@ static const JNINativeMethod gAudioCapsMethods[] = {
     {"native_isSampleRateSupported", "(I)Z", (void *)android_media_AudioCapabilities_isSampleRateSupported}
 };
 
+static const JNINativeMethod gPerformancePointMethods[] = {
+    {"native_covers", "(Landroid/media/MediaCodecInfo$VideoCapabilities$PerformancePoint)Z", (void *)android_media_VideoCapabilities_PerformancePoint_covers},
+    {"native_equals", "(Landroid/media/MediaCodecInfo$VideoCapabilities$PerformancePoint)Z", (void *)android_media_VideoCapabilities_PerformancePoint_equals},
+    {"native_toString", "()Ljava/lang/String", (void *)android_media_VideoCapabilities_PerformancePoint_toString}
+};
+
 int register_android_media_CodecCapabilities(JNIEnv *env) {
     int result = AndroidRuntime::registerNativeMethods(env, "android/media/MediaCodecInfo$AudioCapabilities",
             gAudioCapsMethods, NELEM(gAudioCapsMethods));
+    if (result != JNI_OK) {
+        return result;
+    }
+
+    result = AndroidRuntime::registerNativeMethods(env, "android/media/MediaCodecInfo$VideoCapabilities$PerformancePoint",
+            gPerformancePointMethods, NELEM(gPerformancePointMethods));
     if (result != JNI_OK) {
         return result;
     }

VoltageOS/frameworks_base_new

Test: MediaCodecCapabilitiesTest
Change-Id: I315ead74b1fbc97f1684641aa0572d4e6e9a518c

media/java/android/media/MediaCodecInfo.java
@@ -2388,6 +2388,24 @@ public final class MediaCodecInfo {
             }
 
             /**
+             * Width in macroblocks.
+             *
+             * @hide
+             */
+            /** package private */ int getWidth() {
+                return mWidth;
+            }
+
+            /**
+             * Height in macroblocks.
+             *
+             * @hide
+             */
+            /** package private */ int getHeight() {
+                return mHeight;
+            }
+
+            /**
              * Maximum frame rate in frames per second.
              *
              * @hide
@@ -2405,6 +2423,24 @@ public final class MediaCodecInfo {
             @TestApi
             public long getMaxMacroBlockRate() {
                 return mMaxMacroBlockRate;
+            }
+
+            /**
+             * Codec block width in macroblocks.
+             *
+             * @hide
+             */
+            /** package private */ int getBlockWidth() {
+                return mBlockSize.getWidth();
+            }
+
+            /**
+             * Codec block height in macroblocks.
+             *
+             * @hide
+             */
+            /** package private */ int getBlockHeight() {
+                return mBlockSize.getHeight();
             }
 
             /** Convert to a debug string */
@@ -2494,6 +2530,20 @@ public final class MediaCodecInfo {
                 this(width, height, frameRate, frameRate /* maxFrameRate */, new Size(16, 16));
             }
 
+            /* package private */ PerformancePoint(int width, int height, int maxFrameRate,
+                    long maxMacroBlockRate, int blockSizeWidth, int blockSizeHeight) {
+                mWidth = width;
+                mHeight = height;
+                mMaxFrameRate = maxFrameRate;
+                mMaxMacroBlockRate = maxMacroBlockRate;
+                mBlockSize = new Size(blockSizeWidth, blockSizeHeight);
+            }
+
+            private PerformancePoint(PerformancePoint pp) {
+                this(pp.mWidth, pp.mHeight, pp.mMaxFrameRate, pp.mMaxMacroBlockRate,
+                        pp.mBlockSize.getWidth(), pp.mBlockSize.getHeight());
+            }
+
             /** Saturates a long value to int */
             private int saturateLongToInt(long value) {
                 if (value < Integer.MIN_VALUE) {
@@ -2547,14 +2597,18 @@ public final class MediaCodecInfo {
              * @return {@code true} if the performance point covers the other.
              */
             public boolean covers(@NonNull PerformancePoint other) {
-                // convert performance points to common block size
-                Size commonSize = getCommonBlockSize(other);
-                PerformancePoint aligned = new PerformancePoint(this, commonSize);
-                PerformancePoint otherAligned = new PerformancePoint(other, commonSize);
+                if (GetFlag(() -> android.media.codec.Flags.nativeCapabilites())) {
+                    return native_covers(other);
+                } else {
+                    // convert performance points to common block size
+                    Size commonSize = getCommonBlockSize(other);
+                    PerformancePoint aligned = new PerformancePoint(this, commonSize);
+                    PerformancePoint otherAligned = new PerformancePoint(other, commonSize);
 
-                return (aligned.getMaxMacroBlocks() >= otherAligned.getMaxMacroBlocks()
-                        && aligned.mMaxFrameRate >= otherAligned.mMaxFrameRate
-                        && aligned.mMaxMacroBlockRate >= otherAligned.mMaxMacroBlockRate);
+                    return (aligned.getMaxMacroBlocks() >= otherAligned.getMaxMacroBlocks()
+                            && aligned.mMaxFrameRate >= otherAligned.mMaxFrameRate
+                            && aligned.mMaxMacroBlockRate >= otherAligned.mMaxMacroBlockRate);
+                }
             }
 
             private @NonNull Size getCommonBlockSize(@NonNull PerformancePoint other) {
@@ -2568,15 +2622,26 @@ public final class MediaCodecInfo {
                 if (o instanceof PerformancePoint) {
                     // convert performance points to common block size
                     PerformancePoint other = (PerformancePoint)o;
-                    Size commonSize = getCommonBlockSize(other);
-                    PerformancePoint aligned = new PerformancePoint(this, commonSize);
-                    PerformancePoint otherAligned = new PerformancePoint(other, commonSize);
+                    if (GetFlag(() -> android.media.codec.Flags.nativeCapabilites())) {
+                        return native_equals(other);
+                    } else {
+                        Size commonSize = getCommonBlockSize(other);
+                        PerformancePoint aligned = new PerformancePoint(this, commonSize);
+                        PerformancePoint otherAligned = new PerformancePoint(other, commonSize);
 
-                    return (aligned.getMaxMacroBlocks() == otherAligned.getMaxMacroBlocks()
-                            && aligned.mMaxFrameRate == otherAligned.mMaxFrameRate
-                            && aligned.mMaxMacroBlockRate == otherAligned.mMaxMacroBlockRate);
+                        return (aligned.getMaxMacroBlocks() == otherAligned.getMaxMacroBlocks()
+                                && aligned.mMaxFrameRate == otherAligned.mMaxFrameRate
+                                && aligned.mMaxMacroBlockRate == otherAligned.mMaxMacroBlockRate);
+                    }
                 }
                 return false;
+            }
+
+            private native boolean native_covers(PerformancePoint other);
+            private native boolean native_equals(PerformancePoint other);
+
+            static {
+                System.loadLibrary("media_jni");
             }
 
             /** 480p 24fps */
@@ -4994,6 +5059,11 @@ public final class MediaCodecInfo {
     /* package private */ class GenericHelper {
         private static Range<Integer> constructIntegerRange(int lower, int upper) {
             return Range.create(Integer.valueOf(lower), Integer.valueOf(upper));
+        }
+
+        private static List<VideoCapabilities.PerformancePoint>
+                constructPerformancePointList(VideoCapabilities.PerformancePoint[] array) {
+            return Arrays.asList(array);
         }
     }
 }

media/jni/android_media_CodecCapabilities.cpp
@@ -21,6 +21,7 @@
 #include "jni.h"
 
 #include <media/AudioCapabilities.h>
+#include <media/VideoCapabilities.h>
 #include <media/stagefright/foundation/ADebug.h>
 #include <nativehelper/JNIHelp.h>
 
@@ -37,6 +38,47 @@ static AudioCapabilities* getAudioCapabilities(JNIEnv *env, jobject thiz) {
     AudioCapabilities* const p = (AudioCapabilities*)env->GetLongField(
             thiz, fields.audioCapsContext);
     return p;
+}
+
+// Converters between Java objects and native instances
+
+static VideoCapabilities::PerformancePoint convertToNativePerformancePoint(
+        JNIEnv *env, jobject pp) {
+    if (pp == NULL) {
+        jniThrowException(env, "java/lang/NullPointerException", NULL);
+    }
+
+    jclass clazz = env->FindClass(
+            "android/media/MediaCodecInfo$VideoCapabilities$PerformancePoint");
+    CHECK(clazz != NULL);
+    CHECK(env->IsInstanceOf(pp, clazz));
+
+    jmethodID getWidthID = env->GetMethodID(clazz, "getWidth", "()I");
+    CHECK(getWidthID != NULL);
+    jint width = env->CallIntMethod(pp, getWidthID);
+
+    jmethodID getHeightID = env->GetMethodID(clazz, "getHeight", "()I");
+    CHECK(getHeightID != NULL);
+    jint height = env->CallIntMethod(pp, getHeightID);
+
+    jmethodID getMaxFrameRateID = env->GetMethodID(clazz, "getMaxFrameRate", "()I");
+    CHECK(getMaxFrameRateID != NULL);
+    jint maxFrameRate = env->CallIntMethod(pp, getMaxFrameRateID);
+
+    jmethodID getMaxMacroBlockRateID = env->GetMethodID(clazz, "getMaxMacroBlockRate", "()J");
+    CHECK(getMaxMacroBlockRateID != NULL);
+    jlong maxMacroBlockRate = env->CallLongMethod(pp, getMaxMacroBlockRateID);
+
+    jmethodID getBlockWidthID = env->GetMethodID(clazz, "getBlockWidth", "()I");
+    CHECK(getBlockWidthID != NULL);
+    jint blockWidth = env->CallIntMethod(pp, getBlockWidthID);
+
+    jmethodID getBlockHeightID = env->GetMethodID(clazz, "getBlockHeight", "()I");
+    CHECK(getBlockHeightID != NULL);
+    jint blockHeight = env->CallIntMethod(pp, getBlockHeightID);
+
+    return VideoCapabilities::PerformancePoint(VideoSize(blockWidth, blockHeight),
+            width, height, maxFrameRate, maxMacroBlockRate);
 }
 
 }  // namespace android
@@ -96,6 +138,26 @@ static jboolean android_media_AudioCapabilities_isSampleRateSupported(JNIEnv *en
     return res;
 }
 
+// PerformancePoint
+
+static jboolean android_media_VideoCapabilities_PerformancePoint_covers(JNIEnv *env, jobject thiz,
+        jobject other) {
+    VideoCapabilities::PerformancePoint pp0 = convertToNativePerformancePoint(env, thiz);
+    VideoCapabilities::PerformancePoint pp1 = convertToNativePerformancePoint(env, other);
+
+    bool res = pp0.covers(pp1);
+    return res;
+}
+
+static jboolean android_media_VideoCapabilities_PerformancePoint_equals(JNIEnv *env, jobject thiz,
+        jobject other) {
+    VideoCapabilities::PerformancePoint pp0 = convertToNativePerformancePoint(env, thiz);
+    VideoCapabilities::PerformancePoint pp1 = convertToNativePerformancePoint(env, other);
+
+    bool res = pp0.equals(pp1);
+    return res;
+}
+
 // ----------------------------------------------------------------------------
 
 static const JNINativeMethod gAudioCapsMethods[] = {
@@ -105,10 +167,22 @@ static const JNINativeMethod gAudioCapsMethods[] = {
     {"native_isSampleRateSupported", "(I)Z", (void *)android_media_AudioCapabilities_isSampleRateSupported}
 };
 
+static const JNINativeMethod gPerformancePointMethods[] = {
+    {"native_covers", "(Landroid/media/MediaCodecInfo$VideoCapabilities$PerformancePoint;)Z", (void *)android_media_VideoCapabilities_PerformancePoint_covers},
+    {"native_equals", "(Landroid/media/MediaCodecInfo$VideoCapabilities$PerformancePoint;)Z", (void *)android_media_VideoCapabilities_PerformancePoint_equals},
+};
+
 int register_android_media_CodecCapabilities(JNIEnv *env) {
     int result = AndroidRuntime::registerNativeMethods(env,
             "android/media/MediaCodecInfo$AudioCapabilities$AudioCapsNativeImpl",
             gAudioCapsMethods, NELEM(gAudioCapsMethods));
+    if (result != JNI_OK) {
+        return result;
+    }
+
+    result = AndroidRuntime::registerNativeMethods(env,
+            "android/media/MediaCodecInfo$VideoCapabilities$PerformancePoint",
+            gPerformancePointMethods, NELEM(gPerformancePointMethods));
     if (result != JNI_OK) {
         return result;
     }