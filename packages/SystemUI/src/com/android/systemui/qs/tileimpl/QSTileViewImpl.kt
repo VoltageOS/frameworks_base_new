c588c47e51377d74589ce0a5b0cd0f3ff14fb3b0
Pranav Vashi <neobuddy89@gmail.com>
2025-01-04 22:40:53 +0530

    SystemUI: Do initialize long press QS effects when supported
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileViewImpl.kt
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileViewImpl.kt
@@ -776,9 +776,9 @@ constructor(
         lastIconTint = icon.getColor(state)
 
         // Long-press effects
-        longPressEffect?.qsTile?.state?.handlesLongClick = state.handlesLongClick
+        longPressEffect?.qsTile?.state?.handlesLongClick = hasLongClickEffect
         if (
-            state.handlesLongClick &&
+            hasLongClickEffect &&
                 longPressEffect?.initializeEffect(longPressEffectDuration) == true
         ) {
             showRippleEffect = false
