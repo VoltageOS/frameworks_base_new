bf58d78752f1c28b5d321e68c78bb21f50d99fa5
Pranav Vashi <neobuddy89@gmail.com>
2025-02-08 18:55:52 +0530

    SystemUI: Add less boring heads up option
    * This is complete rewrite for refactored code.
    
    Original Ref: https://github.com/crdroidandroid/android_frameworks_base/commit/f267ab0aaaea53b5940d9a0beafb0be62c82e3ff
    Co-authored-by: ezio84 <brabus84@gmail.com>
    Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7242,6 +7242,12 @@ public final class Settings {
         public static final String QS_DUAL_TONE = "qs_dual_tone";
 
         /**
+         * Whether to show heads up only for dialer and sms apps
+         * @hide
+         */
+        public static final String LESS_BORING_HEADS_UP = "less_boring_heads_up";
+
+        /**
          * Whether to show power menu on LockScreen
          * @hide
          */
