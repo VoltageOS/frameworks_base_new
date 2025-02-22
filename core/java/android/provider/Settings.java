dd539675ba6c814672c1351e11017fd3628371bc
NurKeinNeid <nurkeinneid@derpfest.org>
2025-02-22 16:14:01 +0100

    SystemUI: Add ongoing action chip toggle setting [1/2]
    Change-Id: I2083b5184d9a8233722661e42d7d5f33876fc276
    Signed-off-by: NurKeinNeid <nurkeinneid@derpfest.org>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6931,6 +6931,11 @@ public final class Settings {
          */
         public static final String AUTO_BRIGHTNESS_ONE_SHOT = "auto_brightness_one_shot";
 
+	/**
+         * @hide
+         */
+        public static final String ONGOING_ACTION_CHIP = "ongoing_action_chip";
+
         /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
