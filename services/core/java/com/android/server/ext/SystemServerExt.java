b60f3e711de5584ba0641c0afac1b9626be69677
mrxx0 <0mrxx0@proton.me>
2025-03-15 14:30:23 +0100

    NFC auto turn off
    Squashed with 62a06832a8c0198c141c074e227a4454333d72b5 and
    0c8ac15af818c567891529ac73c5246788f6f1b1.
    
    Co-authored-by: Pratyush <codelab@pratyush.dev>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- services/core/java/com/android/server/ext/SystemServerExt.java
+++ services/core/java/com/android/server/ext/SystemServerExt.java
@@ -36,5 +36,7 @@ public final class SystemServerExt {
     void initBgThread() {
         WifiAutoOff.maybeInit(this);
         BluetoothAutoOff.maybeInit(this);
+        NfcAutoOff.maybeInit(this);
+
     }
 }
