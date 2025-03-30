3090863cea4a1bf9fcba90f56085b875200c1999
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2025-03-30 18:16:30 +0300

    don't allow enabling the instant apps feature
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -11915,6 +11915,7 @@ public final class Settings {
          */
         @SystemApi
         @Readable
+        @Protected(immutableValue = "0")
         public static final String INSTANT_APPS_ENABLED = "instant_apps_enabled";
 
         /**
@@ -17864,6 +17865,7 @@ public final class Settings {
          * @hide
          */
         @Readable
+        @Protected(immutableValue = "0")
         public static final String ENABLE_EPHEMERAL_FEATURE = "enable_ephemeral_feature";
 
         /**
