crdroidandroid/android_frameworks_base

SystemUI: Add pixel style lockscreen weather based OmniJaws
* Similar to SmartSpace but more consistent with keyguard status area.
* Complete rewrite of omni implementation on Android 11.

Ref:
https://github.com/crdroidandroid/android_frameworks_base/commit/97734230d0c1f92f5bc87ff8a515b198d92cdd15
https://github.com/crdroidandroid/android_frameworks_base/commit/05fae49a27afdc68ff9747cbe03de7da0d26bb52
https://github.com/crdroidandroid/android_frameworks_base/commit/8a923c24fb633b7d9f28e4d78145abc44dbb1350

Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

core/java/android/provider/Settings.java
@@ -5624,6 +5624,16 @@ public final class Settings {
         public static final String LOCKSCREEN_WEATHER_ENABLED = "lockscreen_weather_enabled";
 
         /**
+         * @hide
+         */
+        public static final String LOCKSCREEN_WEATHER_LOCATION = "lockscreen_weather_location";
+
+        /**
+         * @hide
+         */
+        public static final String LOCKSCREEN_WEATHER_STYLE = "lockscreen_weather_style";
+
+        /**
          * Double tap on lockscreen to sleep
          * @hide
          */
@@ -6185,11 +6195,6 @@ public final class Settings {
          * @hide
          */
         public static final String ADAPTIVE_PLAYBACK_TIMEOUT = "adaptive_playback_timeout";
-
-        /**
-         * @hide
-         */
-        public static final String LOCKSCREEN_WEATHER_LOCATION = "lockscreen_weather_location";
 
         /**
          * Statusbar logo

VoltageOS/frameworks_base_new

Change-Id: I138c0dc94f08142f6614659037a501d6ae8909b1
Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>
Co-authored-by: Jayant-Deshmukh <jayantdeshmuk008@gmail.com>
Co-authored-by: rmp22 <195054967+rmp22@users.noreply.github.com>
Signed-off-by: Dmitrii <bankersenator@gmail.com>

core/java/android/provider/Settings.java
@@ -7201,6 +7201,31 @@ public final class Settings {
         public static final String NOTIFICATION_SOUND_VIB_SCREEN_ON = "notification_sound_vib_screen_on";
 
         /**
+         * @hide
+         */
+        public static final String LOCKSCREEN_WEATHER_ENABLED = "lockscreen_weather_enabled";
+
+        /**
+         * @hide
+         */
+        public static final String LOCKSCREEN_WEATHER_LOCATION = "lockscreen_weather_location";
+
+        /**
+         * @hide
+         */
+        public static final String LOCKSCREEN_WEATHER_TEXT = "lockscreen_weather_text";
+
+        /**
+         * @hide
+         */
+        public static final String LOCKSCREEN_WEATHER_WIND_INFO = "lockscreen_weather_wind_info";
+
+        /**
+         * @hide
+         */
+        public static final String LOCKSCREEN_WEATHER_HUMIDITY_INFO = "lockscreen_weather_humidity_info";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *