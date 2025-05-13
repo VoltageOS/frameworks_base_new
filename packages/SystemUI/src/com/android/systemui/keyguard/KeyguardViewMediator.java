25153aa71a5447f5ff1a6c91b6d8de4730566034
LuK1337 <priv.luk@gmail.com>
2025-05-13 14:01:16 +0200

    SystemUI: Skip AOD wake up on display timeout if lock screen is disabled
    Fixes: https://gitlab.com/LineageOS/issues/android/-/issues/8793
    Change-Id: I797127516d6138aa8c3e5e1f03e988695ad1e85b
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
+++ packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
@@ -3327,10 +3327,11 @@ public class KeyguardViewMediator implements CoreStartable,
 
         // It's possible that the device was unlocked (via BOUNCER) while dozing. It's time to
         // wake up.
-        // if (mAodShowing) {
-        //     mPM.wakeUp(mSystemClock.uptimeMillis(), PowerManager.WAKE_REASON_GESTURE,
-        //             "com.android.systemui:BOUNCER_DOZING");
-        // }
+        if (mAodShowing && !mLockPatternUtils.isLockScreenDisabled(
+                mSelectedUserInteractor.getSelectedUserId())) {
+            mPM.wakeUp(mSystemClock.uptimeMillis(), PowerManager.WAKE_REASON_GESTURE,
+                    "com.android.systemui:BOUNCER_DOZING");
+        }
 
         synchronized (KeyguardViewMediator.this) {
             if (DEBUG) Log.d(TAG, "handleHide");
