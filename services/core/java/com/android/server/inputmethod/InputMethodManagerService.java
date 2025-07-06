Variation-LOS/android_frameworks_base

fixup! Introduce high touch polling rate feature control
Change-Id: Ia93a250017e8f8b5991b929747953c00033983c1

services/core/java/com/android/server/inputmethod/InputMethodManagerService.java
@@ -3080,8 +3080,8 @@ public final class InputMethodManagerService implements IInputMethodManagerImpl.
         if (!mLineageHardware.isSupported(LineageHardwareManager.FEATURE_HIGH_TOUCH_POLLING_RATE)) {
             return;
         }
-        final boolean enabled = LineageSettings.System.getInt(mContext.getContentResolver(),
-                LineageSettings.System.HIGH_TOUCH_POLLING_RATE_ENABLE, 0) == 1;
+        final boolean enabled = LineageSettings.System.getIntForUser(mContext.getContentResolver(),
+                LineageSettings.System.HIGH_TOUCH_POLLING_RATE_ENABLE, 0, mCurrentImeUserId) == 1;
         mLineageHardware.set(LineageHardwareManager.FEATURE_HIGH_TOUCH_POLLING_RATE, enabled);
     }
 

VoltageOS/frameworks_base_new

Signed-off-by: Dmitrii <bankersenator@gmail.com>

services/core/java/com/android/server/inputmethod/InputMethodManagerService.java
@@ -3093,8 +3093,10 @@ public final class InputMethodManagerService implements IInputMethodManagerImpl.
         if (!mLineageHardware.isSupported(LineageHardwareManager.FEATURE_HIGH_TOUCH_POLLING_RATE)) {
             return;
         }
-        final boolean enabled = Settings.System.getInt(mContext.getContentResolver(),
-                Settings.System.HIGH_TOUCH_POLLING_RATE_ENABLE, 0) == 1;
+        // If user is a profile, use preference of its parent profile.
+        final int profileParentUserId = mUserManagerInternal.getProfileParentId(mCurrentImeUserId);
+        final boolean enabled = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.HIGH_TOUCH_POLLING_RATE_ENABLE, 0, profileParentUserId) == 1;
         mLineageHardware.set(LineageHardwareManager.FEATURE_HIGH_TOUCH_POLLING_RATE, enabled);
     }
 