6d9709a70618b233cf783ae2f73ec9dc65f3fd5f
Dmitrii <bankersenator@gmail.com>
2025-07-20 16:53:44 +0000

    Revert "gmscompat: deprivilege GmsCore credential provider services"
    This reverts commit 55b99d17c2538391080790fdc880a3692e53884f.
    
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- services/core/java/com/android/server/pm/ext/GmsCoreHooks.java
+++ services/core/java/com/android/server/pm/ext/GmsCoreHooks.java
@@ -7,11 +7,9 @@ import android.content.pm.PackageManagerInternal;
 import android.content.pm.ServiceInfo;
 import android.ext.PackageId;
 import android.os.SystemProperties;
-import android.service.credentials.CredentialProviderService;
 
 import com.android.internal.gmscompat.GmcMediaProjectionService;
 import com.android.internal.gmscompat.GmsHooks;
-import com.android.internal.pm.pkg.component.ParsedIntentInfo;
 import com.android.internal.pm.pkg.component.ParsedPermission;
 import com.android.internal.pm.pkg.component.ParsedService;
 import com.android.internal.pm.pkg.component.ParsedServiceImpl;
@@ -57,19 +55,6 @@ class GmsCoreHooks extends PackageHooks {
         @Override
         public boolean shouldSkipPermissionDefinition(ParsedPermission p) {
             return shouldSkipPermissionDefinition(p.getName());
-        }
-
-        @Override
-        public void amendParsedService(ParsedServiceImpl s) {
-            super.amendParsedService(s);
-
-            if (android.Manifest.permission.BIND_CREDENTIAL_PROVIDER_SERVICE.equals(s.getPermission())) {
-                for (ParsedIntentInfo intentInfo : s.getIntents()) {
-                    // SYSTEM_SERVICE_INTERFACE is allowed only for preinstalled providers
-                    intentInfo.getIntentFilter().replaceAction(CredentialProviderService.SYSTEM_SERVICE_INTERFACE,
-                            CredentialProviderService.SERVICE_INTERFACE);
-                }
-            }
         }
 
         static boolean shouldSkipPermissionDefinition(String name) {
