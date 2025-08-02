06158b4a1fbd0de5248a2858fdbd0d7f17488b9c
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-08-02 19:12:31 +0800

    [PERF] activity resume boost
    Change-Id: Ie844a41e634c456aca03455893d5bd8605cff95a
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -19712,7 +19712,7 @@ public class ActivityManagerService extends IActivityManager.Stub
         String path = null;
         String cpuset = null;
         String bgCpuset = SystemProperties.get("persist.sys.voltage_cpu_limit_bg", "0-1");
-        String fgCpuSet = SystemProperties.get("persist.sys.voltage_cpu_unlimit_ui", "0-7");
+        String fgCpuSet = SystemProperties.get("persist.sys.voltage_cpu_limit_ui", "0-4");
 
         switch (cgroup) {
             case "bg":
@@ -19729,6 +19729,10 @@ public class ActivityManagerService extends IActivityManager.Stub
                 break;
             case "sys-bg":
                 path = "/dev/cpuset/system-background/cpus";
+                cpuset = bgCpuset;
+                break;
+            case "cam":
+                path = "/dev/cpuset/camera-daemon/cpus";
                 cpuset = bgCpuset;
                 break;
             default:
@@ -19888,7 +19892,8 @@ public class ActivityManagerService extends IActivityManager.Stub
         setPerformanceMode(true, reason);
 
         if (reason != null && reason.toLowerCase().contains("launch")) {
-            adjustCpusetCpus("fg", duration);
+            String fgboost = SystemProperties.get("persist.sys.voltage_cpu_unlimit_ui", "0-7");
+            adjustCpusetCpus("/dev/cpuset/foreground/cpus", fgboost, duration);
         }
 
         adjustCpusetCpus("bg", duration);
