d1de5fef5718ee5776bb35cf2f476f19de9c2663
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-08-22 17:47:49 +0800

    Revert "[PERF] increase system server priority"
    This reverts commit 20a6de574157183d3457611debc0671a2aa2fc44.
    
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -917,7 +917,7 @@ public final class SystemServer implements Dumpable {
 
             // Prepare the main looper thread (this thread).
             android.os.Process.setThreadPriority(
-                    android.os.Process.THREAD_GROUP_TOP_APP);
+                    android.os.Process.THREAD_PRIORITY_FOREGROUND);
             android.os.Process.setCanSelfBackground(false);
             Looper.prepareMainLooper();
             Looper.getMainLooper().setSlowLogThresholdMs(
