AxionAOSP/android_frameworks_base

[SERVICES] delayed boot completed notifier
delay to avoid OEM init overwrites

Change-Id: I7e1f904d2dd0c1b810657555bc2e050599f3bc29
Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

services/core/java/com/android/server/am/ActivityManagerService.java
@@ -5281,6 +5281,10 @@ public class ActivityManagerService extends IActivityManager.Stub
             ProcessList.startPsiMonitoringAfterBoot();
             initTaskProfiles();
             updateExtraFree();
+            
+            mHandler.postDelayed(() -> {
+                SystemProperties.set("persist.sys.axion_boot_completed", "1");
+            }, 5000);
 
             mUserController.onBootComplete(
                     new IIntentReceiver.Stub() {

VoltageOS/frameworks_base_new

Signed-off-by: Dmitrii <bankersenator@gmail.com>

services/core/java/com/android/server/am/ActivityManagerService.java
@@ -5288,6 +5288,10 @@ public class ActivityManagerService extends IActivityManager.Stub
             ProcessList.startPsiMonitoringAfterBoot();
             initTaskProfiles();
 
+            mHandler.postDelayed(() -> {
+                SystemProperties.set("persist.sys.voltage_boot_completed", "1");
+            }, 5000);
+
             mUserController.onBootComplete(
                     new IIntentReceiver.Stub() {
                         @Override