VoltageOS/frameworks_base_new

commit dadd4a373fc6b60fe6579e34f528987a52610976

Do not use /data/apex/active
With mount_before_data, installed apexes will be mounted from virtual
block devices (device-mapper devices to be precise). Hence the paths
would look like /dev/block/dm-7, not /data/apex/active/...

Hence, checking if the APEX path is not from the preinstalled location
(e.g. /vendor/apex) is fixed in this CL.

For the same reason, deleting those installed APEX files in cleanUp() is
fixed. In fact, these files don't need to be deleted because once the
preinstalled apexes are deleted, those dangling data apexes are deleted
by apexd after reboot.

Bug: 430161238
Flag: com.android.apex.flags.mount_before_data
Test: StagedRollbackTest, StagedInstallInternalTest
Change-Id: I7f0bdc4ffa6f9179d83f30846eef752ab2d163b4

tests/StagedInstallTest/src/com/android/tests/stagedinstallinternal/host/StagedInstallInternalTest.java
@@ -93,12 +93,11 @@ public class StagedInstallInternalTest extends BaseHostJUnit4Test {
         } catch (AssertionError e) {
             Log.e(TAG, e);
         }
+        // Delete test APEXes from the preinstalled partitions and test-only sysconfig.
+        // Note that installed APEX files will be deleted by apexd after reboot.
         deleteFiles("/system/apex/" + APK_IN_APEX_TESTAPEX_NAME + "*.apex",
-                "/data/apex/active/" + APK_IN_APEX_TESTAPEX_NAME + "*.apex",
-                "/data/apex/active/" + SHIM_APEX_PACKAGE_NAME + "*.apex",
                 "/system/apex/test.rebootless_apex_v*.apex",
                 "/vendor/apex/test.rebootless_apex_v*.apex",
-                "/data/apex/active/test.apex.rebootless*.apex",
                 "/system/app/TestApp/TestAppAv1.apk",
                 TEST_VENDOR_APEX_ALLOW_LIST);
     }

tests/StagedInstallTest/app/src/com/android/tests/stagedinstallinternal/StagedInstallInternalTest.java
@@ -465,7 +465,7 @@ public class StagedInstallInternalTest {
                     .isEqualTo(ApplicationInfo.FLAG_UPDATED_SYSTEM_APP);
             assertThat(apex.applicationInfo.flags & ApplicationInfo.FLAG_INSTALLED)
                     .isEqualTo(ApplicationInfo.FLAG_INSTALLED);
-            assertThat(apex.applicationInfo.sourceDir).startsWith("/data/apex/active");
+            assertThat(apex.applicationInfo.sourceDir.startsWith("/system/apex")).isFalse();
         }
         {
             PackageInfo apex = pm.getPackageInfo("test.apex.rebootless",
@@ -489,7 +489,7 @@ public class StagedInstallInternalTest {
                     .isEqualTo(ApplicationInfo.FLAG_UPDATED_SYSTEM_APP);
             assertThat(apex.applicationInfo.flags & ApplicationInfo.FLAG_INSTALLED)
                     .isEqualTo(ApplicationInfo.FLAG_INSTALLED);
-            assertThat(apex.applicationInfo.sourceDir).startsWith("/data/apex/active");
+            assertThat(apex.applicationInfo.sourceDir.startsWith("/system/apex")).isFalse();
         }
         {
             PackageInfo apex = pm.getPackageInfo("test.apex.rebootless",
@@ -609,7 +609,7 @@ public class StagedInstallInternalTest {
         assertThat(pi.getLongVersionCode()).isEqualTo(2);
         assertThat(pi.applicationInfo.privateFlags & ApplicationInfo.PRIVATE_FLAG_VENDOR)
                 .isEqualTo(ApplicationInfo.PRIVATE_FLAG_VENDOR);
-        assertThat(pi.applicationInfo.sourceDir).startsWith("/data/apex");
+        assertThat(pi.applicationInfo.sourceDir.startsWith("/vendor/apex")).isFalse();
     }
 
     private IPackageManagerNative getPackageManagerNative() {

tests/RollbackTest/StagedRollbackTest/src/com/android/tests/rollback/host/StagedRollbackTest.java
@@ -77,8 +77,7 @@ public class StagedRollbackTest extends BaseHostJUnit4Test {
 
     @Before
     public void setUp() throws Exception {
-        deleteFiles("/system/apex/" + APK_IN_APEX_TESTAPEX_NAME + "*.apex",
-                "/data/apex/active/" + APK_IN_APEX_TESTAPEX_NAME + "*.apex");
+        deleteFiles("/system/apex/" + APK_IN_APEX_TESTAPEX_NAME + "*.apex");
         runPhase("expireRollbacks");
         mLogger.start(getDevice());
         getDevice().uninstallPackage("com.android.cts.install.lib.testapp.A");
@@ -94,11 +93,9 @@ public class StagedRollbackTest extends BaseHostJUnit4Test {
         mLogger.stop();
         runPhase("expireRollbacks");
         deleteFiles("/system/apex/" + APK_IN_APEX_TESTAPEX_NAME + "*.apex",
-                "/data/apex/active/" + APK_IN_APEX_TESTAPEX_NAME + "*.apex",
                 apexDataDirDeSys(APK_IN_APEX_TESTAPEX_NAME) + "*",
                 apexDataDirCe(APK_IN_APEX_TESTAPEX_NAME, 0) + "*",
-                "/system/apex/test.rebootless_apex_v*.apex",
-                "/data/apex/active/test.apex.rebootless*.apex");
+                "/system/apex/test.rebootless_apex_v*.apex");
     }
 
     /**

commit 3a0815f71c77d33299470c9350569f893aed63c9

Uninstall SHIM AEPX after test run
This is a regression from dadd4a373fc6b60fe6579e34f528987a52610976.

CleanUp() deletes "preinstalled" test apexes, which should delete
"installed" test apexes after reboot. However, since the SHIM APEX is
not deleted, the installed SHIM APEX remains after cleanup. This can
break other tests which assume that the SHIM APEX is not updated.

This change fixes the regression by explicitly "uninstall" SHIM APEX.

Bug: 445030890
Test: StagedInstallInternalTest
Flag: com.android.apex.flags.mount_before_data
Change-Id: Iae2a92c63f6bd1d5f546664ef46da9945d7e7c1b

tests/StagedInstallTest/src/com/android/tests/stagedinstallinternal/host/StagedInstallInternalTest.java
@@ -31,6 +31,7 @@ import com.android.compatibility.common.tradefed.build.CompatibilityBuildHelper;
 import com.android.ddmlib.Log;
 import com.android.tests.rollback.host.AbandonSessionsRule;
 import com.android.tradefed.device.DeviceNotAvailableException;
+import com.android.tradefed.device.ITestDevice;
 import com.android.tradefed.device.PackageInfo;
 import com.android.tradefed.testtype.DeviceJUnit4ClassRunner;
 import com.android.tradefed.testtype.junit4.BaseHostJUnit4Test;
@@ -48,6 +49,7 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RunWith(DeviceJUnit4ClassRunner.class)
@@ -93,13 +95,7 @@ public class StagedInstallInternalTest extends BaseHostJUnit4Test {
         } catch (AssertionError e) {
             Log.e(TAG, e);
         }
-        // Delete test APEXes from the preinstalled partitions and test-only sysconfig.
-        // Note that installed APEX files will be deleted by apexd after reboot.
-        deleteFiles("/system/apex/" + APK_IN_APEX_TESTAPEX_NAME + "*.apex",
-                "/system/apex/test.rebootless_apex_v*.apex",
-                "/vendor/apex/test.rebootless_apex_v*.apex",
-                "/system/app/TestApp/TestAppAv1.apk",
-                TEST_VENDOR_APEX_ALLOW_LIST);
+        deleteFilesAndReboot();
     }
 
     @Before
@@ -113,21 +109,28 @@ public class StagedInstallInternalTest extends BaseHostJUnit4Test {
     }
 
     /**
-     * Deletes files and reboots the device if necessary.
-     * @param files the paths of files which might contain wildcards
+     * Delete test APEXes from the preinstalled partitions and test-only sysconfig.
+     * Note that installed APEX files will be deleted by apexd after reboot except SHIM APEX.
+     * Hence, SHIM APEX should be uninstalled instead.
      */
-    private void deleteFiles(String... files) throws Exception {
+    private void deleteFilesAndReboot() throws Exception {
+        // paths of files to delete, which might contain wildcards
+        String[] files = {"/system/apex/" + APK_IN_APEX_TESTAPEX_NAME + "*.apex",
+                "/system/apex/test.rebootless_apex_v*.apex",
+                "/vendor/apex/test.rebootless_apex_v*.apex",
+                "/system/app/TestApp/TestAppAv1.apk",
+                TEST_VENDOR_APEX_ALLOW_LIST};
         if (!getDevice().isAdbRoot()) {
             getDevice().enableAdbRoot();
         }
 
-        boolean found = false;
+        boolean filesFound = false;
         boolean remountSystem = false;
         boolean remountVendor = false;
         for (String file : files) {
             CommandResult result = getDevice().executeShellV2Command("ls " + file);
             if (result.getStatus() == CommandStatus.SUCCESS) {
-                found = true;
+                filesFound = true;
                 if (file.startsWith("/system")) {
                     remountSystem = true;
                 } else if (file.startsWith("/vendor")) {
@@ -136,7 +139,7 @@ public class StagedInstallInternalTest extends BaseHostJUnit4Test {
             }
         }
 
-        if (found) {
+        if (filesFound) {
             if (remountSystem) {
                 getDevice().remountSystemWritable();
             }
@@ -146,8 +149,27 @@ public class StagedInstallInternalTest extends BaseHostJUnit4Test {
             for (String file : files) {
                 getDevice().executeShellCommand("rm -rf " + file);
             }
+        }
+
+        boolean shimApexUninstalled = false;
+        final ITestDevice.ApexInfo shimApex = getShimApex().orElseThrow(
+                () -> new AssertionError("Can't find " + SHIM_APEX_PACKAGE_NAME));
+        if (!shimApex.sourceDir.startsWith("/system")) {
+            getDevice().uninstallPackage(SHIM_APEX_PACKAGE_NAME);
+            shimApexUninstalled = true;
+        }
+
+        if (filesFound || shimApexUninstalled) {
             getDevice().reboot();
         }
+    }
+
+    /**
+     * Returns the active shim apex as optional.
+     */
+    public Optional<ITestDevice.ApexInfo> getShimApex() throws DeviceNotAvailableException {
+        return getDevice().getActiveApexes().stream().filter(
+                apex -> apex.name.equals(SHIM_APEX_PACKAGE_NAME)).findAny();
     }
 
     private void pushTestApex(String fileName) throws Exception {