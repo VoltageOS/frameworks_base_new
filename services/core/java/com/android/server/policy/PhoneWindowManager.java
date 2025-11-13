Ashwin4RC/android_frameworks_base

Allow controlling device wake/sleep from lid events
Change-Id: I77d31c70d6a523398575474e26581189af10925b

services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -659,6 +659,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     // Behavior of Back button while in-call and screen on
     int mIncallBackBehavior;
 
+    // Should device react to a lid event
+    boolean mEnableLidControl;
+
     // Whether system navigation keys are enabled
     boolean mSystemNavigationKeysEnabled;
 
@@ -1000,6 +1003,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     UserHandle.USER_ALL);
             resolver.registerContentObserver(LineageSettings.System.getUriFor(
                     LineageSettings.System.VOLUME_ANSWER_CALL), false, this,
+                    UserHandle.USER_ALL);
+            resolver.registerContentObserver(LineageSettings.System.getUriFor(
+                    LineageSettings.System.LID_CONTROL_SLEEP), false, this,
                     UserHandle.USER_ALL);
 
             updateSettings();
@@ -1419,7 +1425,10 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     }
 
     private int getLidBehavior() {
-        return Settings.Global.getInt(mContext.getContentResolver(),
+        if(!mEnableLidControl)
+            return LID_BEHAVIOR_NONE;
+        else
+            return Settings.Global.getInt(mContext.getContentResolver(),
                 Settings.Global.LID_BEHAVIOR, LID_BEHAVIOR_NONE);
     }
 
@@ -3058,6 +3067,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                             UserHandle.USER_CURRENT) == 1;
             mCameraLaunch = LineageSettings.System.getIntForUser(resolver,
                     LineageSettings.System.CAMERA_LAUNCH, 0,
+                    UserHandle.USER_CURRENT) == 1;
+            mEnableLidControl = LineageSettings.System.getIntForUser(resolver,
+                    LineageSettings.System.LID_CONTROL_SLEEP, 0,
                     UserHandle.USER_CURRENT) == 1;
 
             // Configure wake gesture.

VoltageOS/frameworks_base_new

Signed-off-by: Dmitrii <bankersenator@gmail.com>

services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -707,6 +707,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     // Behavior of Back button while in-call and screen on
     int mIncallBackBehavior;
 
+    // Should device react to a lid event
+    boolean mEnableLidControl;
+
     // Whether system navigation keys are enabled
     boolean mSystemNavigationKeysEnabled;
 
@@ -1048,6 +1051,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     UserHandle.USER_ALL);
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.DOZE_TRIGGER_DOUBLETAP), false, this,
+                    UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.LID_CONTROL_SLEEP), false, this,
                     UserHandle.USER_ALL);
             if (mLineageHardware != null && mLineageHardware.isSupported(LineageHardwareManager.FEATURE_KEY_SWAP)) {
                 resolver.registerContentObserver(Settings.Secure.getUriFor(
@@ -1575,6 +1581,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     }
 
     private int getLidBehavior() {
+        if (!mEnableLidControl) {
+            return LID_BEHAVIOR_NONE;
+        }
         return Settings.Global.getInt(mContext.getContentResolver(),
                 Settings.Global.LID_BEHAVIOR, LID_BEHAVIOR_NONE);
     }
@@ -3310,11 +3319,13 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             }
 
 
-            // volume rocker wake
             mVolumeRockerWake = Settings.System.getIntForUser(resolver,
                     Settings.System.VOLUME_ROCKER_WAKE, 0, UserHandle.USER_CURRENT) != 0;
             mClickPartialScreenshot = Settings.System.getIntForUser(resolver,
                     Settings.System.CLICK_PARTIAL_SCREENSHOT, 0,
+                    UserHandle.USER_CURRENT) == 1;
+            mEnableLidControl = Settings.System.getIntForUser(resolver,
+                    Settings.System.LID_CONTROL_SLEEP, 0,
                     UserHandle.USER_CURRENT) == 1;
 
             // Configure wake gesture.