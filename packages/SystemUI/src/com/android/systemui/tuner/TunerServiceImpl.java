c3579d01df3ffaf84cd010c7a8c2da4a54709ec6
Pranav Vashi <neobuddy89@gmail.com>
2025-11-26 03:09:32 +0530

    SystemUI: TunerServiceImpl: Safe concurrent iteration and modification
    Fixes: https://github.com/crdroidandroid/issue_tracker/issues/830
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- packages/SystemUI/src/com/android/systemui/tuner/TunerServiceImpl.java
+++ packages/SystemUI/src/com/android/systemui/tuner/TunerServiceImpl.java
@@ -54,6 +54,7 @@ import dagger.Lazy;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArraySet;
 
 import javax.inject.Inject;
 
@@ -303,7 +304,7 @@ public class TunerServiceImpl extends TunerService {
 
     private void addTunable(Tunable tunable, String key) {
         if (!mTunableLookup.containsKey(key)) {
-            mTunableLookup.put(key, new ArraySet<Tunable>());
+            mTunableLookup.put(key, new CopyOnWriteArraySet<Tunable>());
         }
         mTunableLookup.get(key).add(tunable);
         if (LeakDetector.ENABLED) {
