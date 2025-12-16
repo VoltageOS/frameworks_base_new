999f9a14c6b211ad818d3edb67681c63f309c40a
Dmitrii <bankersenator@gmail.com>
2025-12-16 04:41:06 +0000

    (WIP) ProgressIndicator: compose refactor
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
@@ -1273,17 +1273,14 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces {
                         mShadeSurface.updateExpansionAndVisibility();
                         setBouncerShowingForStatusBarComponents(mBouncerShowing);
                         checkBarModes();
-                        mOnGoingActionProgressController =
-                             new OnGoingActionProgressController(
-                                     mContext,
+                        if (!StatusBarRootModernization.isEnabled()) {
+                            mOnGoingActionProgressController =
+                                 new OnGoingActionProgressController(
+                                         mContext,
                                      statusBarViewController.getOngoingActionProgressGroup(), mNotificationListener,
                                      mKeyguardStateController, mHeadsUpManager);
+                        }
                     });
-        }
-        if (!StatusBarRootModernization.isEnabled() && !StatusBarConnectedDisplays.isEnabled()) {
-            // When the flag is on, we register the fragment as a core startable and this is not
-            // needed
-            mStatusBarInitializer.initializeStatusBar();
         }
 
         mShadeTouchableRegionManager.setup(getNotificationShadeWindowView());
