4d033bbf098d0cdb48a706111599193b3c9c181c
LuK1337 <priv.luk@gmail.com>
2025-12-28 22:49:08 +0100

    SystemUI: Bring back good ol' circle battery style once again
    Change-Id: I0357c8d71f15f71d86bf0e4fab8b7f4487e6efb3
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7014,6 +7014,17 @@ public final class Settings {
         public static final String STATUS_BAR_SHOW_BATTERY_PERCENT = "status_bar_show_battery_percent";
 
         /**
+         * Style of battery to show.
+         * 0 = default
+         * 1 = circle
+         * 2 = text
+         * 3 = circle dotted
+         * @hide
+         */
+        @Readable
+        public static final String STATUS_BAR_BATTERY_STYLE = "status_bar_battery_style";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -131,7 +131,8 @@ public class SystemSettings {
                 Settings.System.CV_DYNAMIC_ENABLED,
                 Settings.System.CV_PREFERRED_INTENSITY,
                 Settings.System.LOCKSCREEN_ROTATION,
-                Settings.System.STATUS_BAR_SHOW_BATTERY_PERCENT
+                Settings.System.STATUS_BAR_SHOW_BATTERY_PERCENT,
+                Settings.System.STATUS_BAR_BATTERY_STYLE
         ));
         if (Flags.backUpSmoothDisplayAndForcePeakRefreshRate()) {
             settings.add(Settings.System.PEAK_REFRESH_RATE);
