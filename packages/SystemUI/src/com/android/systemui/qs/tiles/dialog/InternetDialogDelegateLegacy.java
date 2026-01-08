c0f9ac92884a95269e39345355d9b825cc20f1e0
Dmitrii <bankersenator@gmail.com>
2026-01-08 10:09:16 +0000

    Revert "SystemUI: Fix exception when retrieving signal strength drawable"
    This reverts commit 45adc61dca44f7d7b1f9baa71a024b4169be9f6f.
    
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tiles/dialog/InternetDialogDelegateLegacy.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/dialog/InternetDialogDelegateLegacy.java
@@ -482,7 +482,7 @@ public class InternetDialogDelegateLegacy implements
         internetContent.mCurrentSatelliteState =
                 mInternetDetailsContentController.getCurrentSatelliteState();
         internetContent.mDefaultSubSignalStrengthIcon =
-                mInternetDetailsContentController.getSignalStrengthDrawable(mDefaultDataSubId).mutate();
+                mInternetDetailsContentController.getSignalStrengthDrawable(mDefaultDataSubId);
 
         return internetContent;
     }
