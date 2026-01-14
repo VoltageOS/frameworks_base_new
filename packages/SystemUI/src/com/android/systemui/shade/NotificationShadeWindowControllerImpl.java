LineageOS/android_frameworks_base

SystemUI: Allow enabling lockscreen rotation by default
Change-Id: I710d2635eab197c32abf55b5594d3583608b2846

packages/SystemUI/src/com/android/systemui/shade/NotificationShadeWindowControllerImpl.java
@@ -246,7 +246,9 @@ public class NotificationShadeWindowControllerImpl implements NotificationShadeW
                 Settings.System.ACCELEROMETER_ROTATION, 0) != 0;
         boolean enableLockScreenRotation =
                 LineageSettings.System.getInt(mContext.getContentResolver(),
-                LineageSettings.System.LOCKSCREEN_ROTATION, 0) != 0;
+                LineageSettings.System.LOCKSCREEN_ROTATION,
+                mContext.getResources().getInteger(org.lineageos.platform.internal.R.bool.
+                        config_lockScreenRotationEnabledByDefault) ? 1 : 0) != 0;
         return mKeyguardStateController.isKeyguardScreenRotationAllowed()
                 && (enableLockScreenRotation && enableAccelerometerRotation);
     }

VoltageOS/frameworks_base_new

Signed-off-by: Dmitrii <bankersenator@gmail.com>

packages/SystemUI/src/com/android/systemui/shade/NotificationShadeWindowControllerImpl.java
@@ -293,7 +293,9 @@ public class NotificationShadeWindowControllerImpl implements NotificationShadeW
                 Settings.System.ACCELEROMETER_ROTATION, 0) != 0;
         boolean enableLockScreenRotation =
                 Settings.System.getInt(mContext.getContentResolver(),
-                Settings.System.LOCKSCREEN_ROTATION, 0) != 0;
+                Settings.System.LOCKSCREEN_ROTATION,
+                mContext.getResources().getBoolean(com.android.internal.R.bool.
+                        config_lockScreenRotationEnabledByDefault) ? 1 : 0) != 0;
         return mKeyguardStateController.isKeyguardScreenRotationAllowed()
                 && (enableLockScreenRotation && enableAccelerometerRotation);
     }