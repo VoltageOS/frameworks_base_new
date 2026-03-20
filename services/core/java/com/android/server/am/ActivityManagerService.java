cd3d69cc2439c9edf79062c75b6e7c60381fdddd
luanzhuang <luanzhuang@xiaomi.corp-partner.google.com>
2026-03-20 19:56:55 +0800

    StrictMode triggers a watchdog
    If proc is null, other threads may remain in the “Waiting” state indefinitely within the handleApplicationStrictModeViolation method.
    
    Test: monkey test
    Flag: EXEMPT bugfix
    Bug: 494493385
    
    Change-Id: Ideb98538203f5d0855300424e9aae54358d19a76
    Signed-off-by: luanzhuang <luanzhuang@xiaomi.corp-partner.google.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -1710,7 +1710,9 @@ public class ActivityManagerService extends IActivityManager.Stub
                     HashMap<String, Object> data = (HashMap<String, Object>) msg.obj;
                     synchronized (mProcLock) {
                         ProcessRecord proc = (ProcessRecord) data.get("app");
+                        AppErrorResult res = (AppErrorResult) data.get("result");
                         if (proc == null) {
+                            res.set(0);
                             Slog.e(TAG, "App not found when showing strict mode dialog.");
                             break;
                         }
@@ -1718,7 +1720,6 @@ public class ActivityManagerService extends IActivityManager.Stub
                             Slog.e(TAG, "App already has strict mode dialog: " + proc);
                             return;
                         }
-                        AppErrorResult res = (AppErrorResult) data.get("result");
                         if (mAtmInternal.showStrictModeViolationDialog()) {
                             proc.mErrorState.getDialogController().showViolationDialogs(res);
                         } else {
