1xtAsh/cr_base

SystemUI: Implement RefreshRateManager [1/2]
Includes:
- Per-app refresh rate config
- Extreme refresh rate (Force all apps to run in maximum refresh rate)
- Temp refresh rate allowed (Used in MEMC)
- Refresh rate QS tile

Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>
[nurkeinneid: move to fwb and drop sdk extensions/dependencies]
Co-authored-by: NurKeinNeid <nurkeinneid@derpfest.org>
Signed-off-by: NurKeinNeid <nurkeinneid@derpfest.org>

core/java/org/derpfest/display/IRefreshRateListener.aidl
@@ -1,0 +1,14 @@
+/*
+ * Copyright (C) 2023-2024 The Nameless-AOSP Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.derpfest.display;
+
+/** @hide */
+oneway interface IRefreshRateListener {
+
+    void onRequestedRefreshRate(int refreshRate);
+
+    void onRequestedMemcRefreshRate(int refreshRate);
+}

core/java/org/derpfest/display/IRefreshRateManagerService.aidl
@@ -1,0 +1,42 @@
+/*
+ * Copyright (C) 2023-2024 The Nameless-AOSP Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.derpfest.display;
+
+import org.derpfest.display.IRefreshRateListener;
+
+/** @hide */
+interface IRefreshRateManagerService {
+
+    /* Request MEMC required refresh rate in video apps */
+    void requestMemcRefreshRate(in int refreshRate);
+
+    /* Restore refresh rate after exiting MEMC mode */
+    void clearRequestedMemcRefreshRate();
+
+    /* Get user preferred refresh rate for specific package, return -1 if not set */
+    int getRefreshRateForPackage(in String packageName);
+
+    /* Request user preferred refresh rate for specific package */
+    void setRefreshRateForPackage(in String packageName, in int refreshRate);
+
+    /* Reset user preferred refresh rate for specific package (Follow system) */
+    void unsetRefreshRateForPackage(in String packageName);
+
+    /* Force highest refresh rate in every apps, unless MEMC is running */
+    void setExtremeRefreshRateEnabled(in boolean enabled);
+
+    /* Get user preferred refresh in current app, return -1 if not set */
+    int getRequestedRefreshRate();
+
+    /* Get current MEMC requested refresh rate, return -1 if not set */
+    int getRequestedMemcRefreshRate();
+
+    /* Register listener to listen requested refresh rate change */
+    boolean registerRefreshRateListener(in IRefreshRateListener listener);
+
+    /* Unregister listener to listen requested refresh rate change */
+    boolean unregisterRefreshRateListener(in IRefreshRateListener listener);
+}

VoltageOS/frameworks_base_new

Signed-off-by: Dmitrii <bankersenator@gmail.com>

core/java/com/voltage/display/IRefreshRateListener.aidl
@@ -1,0 +1,14 @@
+/*
+ * Copyright (C) 2023-2024 The Nameless-AOSP Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.voltage.display;
+
+/** @hide */
+oneway interface IRefreshRateListener {
+
+    void onRequestedRefreshRate(int refreshRate);
+
+    void onRequestedMemcRefreshRate(int refreshRate);
+}

core/java/com/voltage/display/IRefreshRateManagerService.aidl
@@ -1,0 +1,42 @@
+/*
+ * Copyright (C) 2023-2024 The Nameless-AOSP Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.voltage.display;
+
+import com.voltage.display.IRefreshRateListener;
+
+/** @hide */
+interface IRefreshRateManagerService {
+
+    /* Request MEMC required refresh rate in video apps */
+    void requestMemcRefreshRate(in int refreshRate);
+
+    /* Restore refresh rate after exiting MEMC mode */
+    void clearRequestedMemcRefreshRate();
+
+    /* Get user preferred refresh rate for specific package, return -1 if not set */
+    int getRefreshRateForPackage(in String packageName);
+
+    /* Request user preferred refresh rate for specific package */
+    void setRefreshRateForPackage(in String packageName, in int refreshRate);
+
+    /* Reset user preferred refresh rate for specific package (Follow system) */
+    void unsetRefreshRateForPackage(in String packageName);
+
+    /* Force highest refresh rate in every apps, unless MEMC is running */
+    void setExtremeRefreshRateEnabled(in boolean enabled);
+
+    /* Get user preferred refresh in current app, return -1 if not set */
+    int getRequestedRefreshRate();
+
+    /* Get current MEMC requested refresh rate, return -1 if not set */
+    int getRequestedMemcRefreshRate();
+
+    /* Register listener to listen requested refresh rate change */
+    boolean registerRefreshRateListener(in IRefreshRateListener listener);
+
+    /* Unregister listener to listen requested refresh rate change */
+    boolean unregisterRefreshRateListener(in IRefreshRateListener listener);
+}